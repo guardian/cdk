b2525ec2561d00f3c9c276534d7018614c61928a
Jamie Lynch <jamie.lynch@guardian.co.uk>
2020-11-19 10:13:23 +0000

    add-linting

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: "@guardian/eslint-config-typescript",
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12,
+    tsconfigRootDir: __dirname,
+    project: ["./tsconfig.eslint.json"],
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/no-inferrable-types": 0,
+    "import/no-namespace": 2,
+  },
+  root: true,
+  ignorePatterns: ["**/*.js", "node_modules"],
+};
