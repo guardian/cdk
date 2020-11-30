2806fbffe150d7464efd3233f24a7b849613861c
Jorge Azevedo <jorge.azevedo@guardian.co.uk>
2020-11-30 17:31:01 +0000

    Implement a more standard interface

--- src/constructs/securitygroup/wazuh.ts
+++ src/constructs/securitygroup/wazuh.ts
@@ -1,19 +1,25 @@
-import type { IVpc } from "@aws-cdk/aws-ec2";
-import { Peer, Port, } from "@aws-cdk/aws-ec2";
+import { Peer, Port } from "@aws-cdk/aws-ec2";
 import type { GuStack } from "../core";
 import { GuSecurityGroup } from "../ec2";
+import type { GuSecurityGroupProps } from "../ec2";
 
 export class GuWazuhAccess extends GuSecurityGroup {
-  constructor(scope: GuStack, vpc: IVpc) {
-
-    super(scope, "WazuhSecurityGroup", {
-      vpc,
+  private static getDefaultProps(): Partial<GuSecurityGroupProps> {
+    return {
       description: "Wazuh agent registration and event logging",
       overrideId: true,
       allowAllOutbound: false,
-      egresses: [{range: Peer.anyIpv4(), port: Port.tcp(1514), description: "wazuh event logging"},
-                 {range: Peer.anyIpv4(), port: Port.tcp(1515), description: "wazuh agent registration"}]
-    });
+      egresses: [
+        { range: Peer.anyIpv4(), port: Port.tcp(1514), description: "wazuh event logging" },
+        { range: Peer.anyIpv4(), port: Port.tcp(1515), description: "wazuh agent registration" },
+      ],
+    };
+  }
 
+  constructor(scope: GuStack, id: string = "WazuhSecurityGroup", props: GuSecurityGroupProps) {
+    super(scope, id, {
+      ...GuWazuhAccess.getDefaultProps(),
+      ...props,
+    });
   }
 }
