e377b5d2142657087854d4e69a2ec650b9516275
Akash Askoolum <akash1810@gmail.com>
2020-12-07 23:05:53 +0000

    tweak build, lint and test scripts to install dependencies only in a CI environment

--- script/build.sh
+++ script/build.sh
@@ -2,5 +2,10 @@
 
 set -e
 
-npm ci
+# only install dependencies when running in CI (currently GitHub Actions)
+# see https://docs.github.com/en/free-pro-team@latest/actions/reference/environment-variables#default-environment-variables
+if [ $CI ] ; then
+  npm ci
+fi
+
 npm run build
