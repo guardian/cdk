c4bb26670fc74ef337e2f5f973200374cdeba31f
Akash Askoolum <akash1810@gmail.com>
2020-12-18 10:38:48 +0000

    make `app` mandatory in a GuStack
    For now, we do not support a multi-app stack so make `app` mandatory for simplicity.

--- test/utils/simple-gu-stack.ts
+++ test/utils/simple-gu-stack.ts
@@ -1,4 +1,4 @@
 import { App } from "@aws-cdk/core";
 import { GuStack } from "../../src/constructs/core";
 
-export const simpleGuStackForTesting: () => GuStack = () => new GuStack(new App());
+export const simpleGuStackForTesting: () => GuStack = () => new GuStack(new App(), "Test", { app: "testing" });
