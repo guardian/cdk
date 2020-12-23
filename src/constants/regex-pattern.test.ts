cd5a41b0d7560f49e1e1e3856ca6d899c6d4c3bf
Akash Askoolum <akash1810@gmail.com>
2020-12-23 12:00:05 +0000

    Add policy to allow sendiing of emails.
    This policy construct will add a parameter to the stack for the sending address, which has regex'd input limiting it to a @theguardian.com email address.

--- src/constants/regex-pattern.test.ts
+++ src/constants/regex-pattern.test.ts
@@ -13,4 +13,16 @@ describe("the regex patterns", () => {
     expect(regex.test("arn:aws:rds:us-east-2:123456789012:db:my-mysql-instance-1")).toBeFalsy();
     expect(regex.test("arn:aws:s3:::examplebucket/my-data/sales-export-2019-q4.json")).toBeTruthy();
   });
+
+  it("should successfully regex against valid emails only", () => {
+    const regex = new RegExp(RegexPattern.GUARDIAN_EMAIL);
+    expect(regex.test("email.address.com")).toBeFalsy();
+    expect(regex.test("email@gmail.com")).toBeFalsy();
+
+    expect(regex.test("email@theguardian.com")).toBeTruthy();
+    expect(regex.test("another.email@theguardian.com")).toBeTruthy();
+
+    expect(regex.test("another.@theguardian.com")).toBeFalsy();
+    expect(regex.test("another1@theguardian.com")).toBeFalsy();
+  });
 });
--- src/constructs/core/parameters.ts
+++ src/constructs/core/parameters.ts
@@ -105,3 +105,13 @@ export class GuS3ObjectArnParameter extends GuStringParameter {
     });
   }
 }
+
+export class GuGuardianEmailSenderParameter extends GuStringParameter {
+  constructor(scope: GuStack, id: string = "EmailSenderAddress", props?: GuNoTypeParameterProps) {
+    super(scope, id, {
+      ...props,
+      allowedPattern: RegexPattern.GUARDIAN_EMAIL,
+      constraintDescription: "Must be an @theguardian.com email address",
+    });
+  }
+}
