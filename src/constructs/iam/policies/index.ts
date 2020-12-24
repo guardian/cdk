a3be8da65c4c9fa4f5f3d470d20090a11692c78e
Akash Askoolum <akash1810@gmail.com>
2020-12-24 12:25:38 +0000

    add dynamodb read and write policies

--- src/constructs/iam/policies/index.ts
+++ src/constructs/iam/policies/index.ts
@@ -1,5 +1,6 @@
 export * from "./base-policy";
 export * from "./describe-ec2";
+export * from "./dynamodb";
 export * from "./log-shipping";
 export * from "./parameter-store-read";
 export * from "./s3-get-object";
