a31ac00137e6a129792f1e9d75faf2b8f73e4c6d
Akash Askoolum <akash1810@gmail.com>
2021-01-13 19:39:04 +0000

    GuGetS3ObjectPolicy extends GuAllowPolicy for simplicity

--- src/constructs/iam/policies/s3-get-object.ts
+++ src/constructs/iam/policies/s3-get-object.ts
@@ -1,26 +1,13 @@
-import { Effect, PolicyStatement } from "@aws-cdk/aws-iam";
 import type { GuStack } from "../../core";
 import type { GuPolicyProps } from "./base-policy";
-import { GuPolicy } from "./base-policy";
+import { GuAllowPolicy } from "./base-policy";
 
 export interface GuGetS3ObjectPolicyProps extends GuPolicyProps {
   bucketName: string;
 }
 
-export const allowGetObjectPolicyStatement: (bucket: string) => { statements: PolicyStatement[] } = (
-  bucketName: string
-) => ({
-  statements: [
-    new PolicyStatement({
-      effect: Effect.ALLOW,
-      actions: ["s3:GetObject"],
-      resources: [`arn:aws:s3:::${bucketName}/*`],
-    }),
-  ],
-});
-
-export class GuGetS3ObjectPolicy extends GuPolicy {
+export class GuGetS3ObjectPolicy extends GuAllowPolicy {
   constructor(scope: GuStack, id: string, props: GuGetS3ObjectPolicyProps) {
-    super(scope, id, { ...allowGetObjectPolicyStatement(props.bucketName), ...props });
+    super(scope, id, { ...props, actions: ["s3:GetObject"], resources: [`arn:aws:s3:::${props.bucketName}/*`] });
   }
 }
