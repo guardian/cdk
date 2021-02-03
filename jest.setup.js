1bba71f4255ceebcb943df15f454b6f1977024c9
Akash Askoolum <akash1810@gmail.com>
2021-02-03 00:38:38 +0000

    mock TrackingTag in tests
    Mocking this means we can make the value static and it doesn't need updating when new versions of the library are published.
    
    Configuring Jest's `setupFilesAfterEnv` means we apply the mock globally. See https://jestjs.io/docs/en/configuration.html#setupfilesafterenv-array.

---
+++ jest.setup.js
@@ -0,0 +1,1 @@
+jest.mock("./src/constants/library-info");
---
+++ src/constants/__mocks__/library-info.ts
@@ -0,0 +1,8 @@
+export const LibraryInfo = {
+  VERSION: "TEST",
+};
+
+export const TrackingTag = {
+  Key: "X-Gu-CDK-Version",
+  Value: LibraryInfo.VERSION,
+};
