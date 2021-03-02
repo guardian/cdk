11df3e725d4e374e3b1ac3c5e2029458f6f83b93
Akash Askoolum <akash1810@gmail.com>
2021-03-02 08:36:56 +0000

    refactor: make SSM policy extend GuAllowPolicy for DRYness

--- src/constructs/iam/policies/ssm.ts
+++ src/constructs/iam/policies/ssm.ts
@@ -1,41 +1,28 @@
-import type { PolicyProps } from "@aws-cdk/aws-iam";
-import { Effect, PolicyStatement } from "@aws-cdk/aws-iam";
 import type { GuStack } from "../../core";
-import type { GuPolicyProps } from "./base-policy";
-import { GuPolicy } from "./base-policy";
+import type { GuNoStatementsPolicyProps } from "./base-policy";
+import { GuAllowPolicy } from "./base-policy";
 
-export const allowSSMRunCommandPolicyStatement = new PolicyStatement({
-  effect: Effect.ALLOW,
-  actions: [
-    "ec2messages:AcknowledgeMessage",
-    "ec2messages:DeleteMessage",
-    "ec2messages:FailMessage",
-    "ec2messages:GetEndpoint",
-    "ec2messages:GetMessages",
-    "ec2messages:SendReply",
-    "ssm:UpdateInstanceInformation",
-    "ssm:ListInstanceAssociations",
-    "ssm:DescribeInstanceProperties",
-    "ssm:DescribeDocumentParameters",
-    "ssmmessages:CreateControlChannel",
-    "ssmmessages:CreateDataChannel",
-    "ssmmessages:OpenControlChannel",
-    "ssmmessages:OpenDataChannel",
-  ],
-  resources: ["*"],
-});
-
-export class GuSSMRunCommandPolicy extends GuPolicy {
-  private static getDefaultProps(): Partial<PolicyProps> {
-    return {
-      policyName: "ssm-run-command-policy",
-      statements: [allowSSMRunCommandPolicyStatement],
-    };
-  }
-
-  constructor(scope: GuStack, id: string = "SSMRunCommandPolicy", props?: GuPolicyProps) {
+export class GuSSMRunCommandPolicy extends GuAllowPolicy {
+  constructor(scope: GuStack, id: string = "SSMRunCommandPolicy", props?: GuNoStatementsPolicyProps) {
     super(scope, id, {
-      ...GuSSMRunCommandPolicy.getDefaultProps(),
+      policyName: "ssm-run-command-policy",
+      resources: ["*"],
+      actions: [
+        "ec2messages:AcknowledgeMessage",
+        "ec2messages:DeleteMessage",
+        "ec2messages:FailMessage",
+        "ec2messages:GetEndpoint",
+        "ec2messages:GetMessages",
+        "ec2messages:SendReply",
+        "ssm:UpdateInstanceInformation",
+        "ssm:ListInstanceAssociations",
+        "ssm:DescribeInstanceProperties",
+        "ssm:DescribeDocumentParameters",
+        "ssmmessages:CreateControlChannel",
+        "ssmmessages:CreateDataChannel",
+        "ssmmessages:OpenControlChannel",
+        "ssmmessages:OpenDataChannel",
+      ],
       ...props,
     });
   }
