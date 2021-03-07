4f29f59f3ad9a717ea56a4412ce25ce0db25bb2b
Akash Askoolum <akash1810@gmail.com>
2021-03-07 13:49:39 +0000

    refactor: adjust GuParameter linting rules
    Updated rules:
    1. Must be defined in the parameters directory
    2. Constructor parameters must not have a default
    3. Constructor parameters must not be optional
    
    If a direct extension of GuParameter:
      Constructors must take three parameters:
        1. First parameter must be called scope and be of type GuStack
        2. Second parameter must be called id and be of type TSStringKeyword
        3. Third parameter must be called id and be of type GuNoTypeParameterProps
    Else:
      Constructors must take one parameter called scope and be of type GuStack

--- eslint/logic/gu-parameter-constructors.js
+++ eslint/logic/gu-parameter-constructors.js
@@ -1,7 +1,17 @@
 /*
-Rules for GuParameters:
+The rules:
+
 1. Must be defined in the parameters directory
-2. Constructors must take one parameter called scope and be of type GuStack
+2. Constructor parameters must not have a default
+3. Constructor parameters must not be optional
+
+If a direct extension of GuParameter:
+  Constructors must take three parameters:
+    1. First parameter must be called scope and be of type GuStack
+    2. Second parameter must be called id and be of type TSStringKeyword
+    3. Third parameter must be called id and be of type GuNoTypeParameterProps
+Else:
+  Constructors must take one parameter called scope and be of type GuStack
  */
 
 const guParameterSuperClassRegex = new RegExp("^Gu[A-Za-z]*Parameter$");
@@ -13,15 +23,72 @@ const isGuParameter = (node) => {
   return superClass ? guParameterSuperClassRegex.test(superClass) : false;
 };
 
-const parseGuParameterConstructor = (context, node) => {
-  const isInCorrectLocation = guParameterDirectoryRegex.test(context.getFilename());
-  if (!isInCorrectLocation) {
+const directlyExtendsGuParameter = (node) => {
+  return node.parent?.parent?.superClass?.name === "GuParameter";
+};
+
+const parseDirectExtensionOfGuParameter = (context, node) => {
+  const { params } = node.value;
+
+  const hasExactlyThreeParams = !Array.isArray(params) || params.length === 3;
+  if (!hasExactlyThreeParams) {
     return context.report({
       node,
-      message: `GuParameters must be defined in the directory ${guParameterDirectory}`,
+      message: "Constructors of classes that extend GuParameter must take exactly three parameters",
+      loc: params.loc,
     });
   }
 
+  params.forEach((param) => {
+    if (param.type === "AssignmentPattern") {
+      return context.report({
+        node,
+        message: "Parameters to constructors of classes that extend GuParameter must not have a default",
+        loc: param.loc,
+      });
+    }
+
+    if (param.optional) {
+      return context.report({
+        node,
+        message: "Parameters to constructors of classes that extend GuParameter must not be optional",
+        loc: param.loc,
+      });
+    }
+  });
+
+  const paramSpec = [
+    { expectedName: "scope", expectedType: "GuStack", humanReadablePosition: "first" },
+    { expectedName: "id", expectedType: "TSStringKeyword", humanReadablePosition: "second" },
+    { expectedName: "props", expectedType: "GuNoTypeParameterProps", humanReadablePosition: "third" },
+  ];
+
+  paramSpec.forEach((spec, index) => {
+    const param = params[index];
+
+    if (param.name !== spec.expectedName) {
+      return context.report({
+        node,
+        message: `The ${spec.humanReadablePosition} parameter of classes that extend GuParameter must be called ${spec.expectedName}`,
+        loc: param.loc,
+      });
+    }
+
+    const paramType = param.typeAnnotation.typeAnnotation.typeName?.name ?? param.typeAnnotation.typeAnnotation.type;
+
+    if (paramType !== spec.expectedType) {
+      return context.report({
+        node,
+        message: `The ${spec.humanReadablePosition} parameter of classes that extend GuParameter must be of type ${spec.expectedType}`,
+        loc: param.loc,
+      });
+    }
+  });
+
+  return null;
+};
+
+const parseOtherGuParameter = (context, node) => {
   const { params } = node.value;
 
   const hasMultipleParameters = !Array.isArray(params) || params.length !== 1;
@@ -35,11 +102,28 @@ const parseGuParameterConstructor = (context, node) => {
 
   const [scope] = params;
 
+  const hasDefault = scope.type === "AssignmentPattern";
+  if (hasDefault) {
+    return context.report({
+      node,
+      message: "Parameters to constructors of classes that extend GuParameter must not have a default",
+      loc: scope.loc,
+    });
+  }
+
+  if (scope.optional) {
+    return context.report({
+      node,
+      message: "Parameters to constructors of classes that extend GuParameter must not be optional",
+      loc: scope.loc,
+    });
+  }
+
   const scopeParamIsCorrectName = scope.name === "scope";
   if (!scopeParamIsCorrectName) {
     return context.report({
       node,
-      message: "The parameter in a GuParameter constructor must be called scope",
+      message: "The first parameter in a GuParameter constructor must be called scope",
       loc: scope.loc,
     });
   }
@@ -54,6 +138,20 @@ const parseGuParameterConstructor = (context, node) => {
   }
 
   return null;
+};
+
+const parseGuParameterConstructor = (context, node) => {
+  const isInCorrectLocation = guParameterDirectoryRegex.test(context.getFilename());
+  if (!isInCorrectLocation) {
+    return context.report({
+      node,
+      message: `GuParameters must be defined in the directory ${guParameterDirectory}`,
+    });
+  }
+
+  return directlyExtendsGuParameter(node)
+    ? parseDirectExtensionOfGuParameter(context, node)
+    : parseOtherGuParameter(context, node);
 };
 
 module.exports = {
