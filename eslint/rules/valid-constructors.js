47c5ff85cd2e8e8ef4c3ae7a42287d57846d3748
Akash Askoolum <akash1810@gmail.com>
2021-03-07 09:59:07 +0000

    chore: create custom linting rules for GuParameters
    Rules for GuParameters:
    1. Must be defined in the parameters directory
    2. Constructors must take one parameter called scope and be of type GuStack

--- eslint/rules/valid-constructors.js
+++ eslint/rules/valid-constructors.js
@@ -33,6 +33,64 @@ const getStandardOrLeftProp = (node, path) => {
   }
 };
 
+const guParameterSuperClassRegex = new RegExp("^Gu[A-Za-z]*Parameter$");
+const guParameterDirectory = "src/constructs/core/parameters";
+const guParameterDirectoryRegex = new RegExp(`${guParameterDirectory}/.+\.ts$`);
+
+const isGuParameter = (node) => {
+  const superClass = node.parent?.parent?.superClass?.name;
+  return superClass ? guParameterSuperClassRegex.test(superClass) : false;
+};
+
+const parseGuParameter = (context, node) => {
+  /*
+  Rules for GuParameters:
+  1. Must be defined in the parameters directory
+  2. Constructors must take one parameter called scope and be of type GuStack
+   */
+
+  const isInCorrectLocation = guParameterDirectoryRegex.test(context.getFilename());
+  if (!isInCorrectLocation) {
+    return context.report({
+      node,
+      message: `GuParameters must be defined in the directory ${guParameterDirectory}`,
+    });
+  }
+
+  const { params } = node.value;
+
+  const hasMultipleParameters = !Array.isArray(params) || params.length !== 1;
+  if (hasMultipleParameters) {
+    return context.report({
+      node,
+      message: "GuParameter constructors must only take one parameter",
+      loc: params.loc,
+    });
+  }
+
+  const [scope] = params;
+
+  const scopeParamIsCorrectName = scope.name === "scope";
+  if (!scopeParamIsCorrectName) {
+    return context.report({
+      node,
+      message: "The parameter in a GuParameter constructor must be called scope",
+      loc: scope.loc,
+    });
+  }
+
+  const scopeParamIsCorrectType = scope.typeAnnotation.typeAnnotation.typeName.name === "GuStack";
+  if (!scopeParamIsCorrectType) {
+    return context.report({
+      node,
+      message: "The parameter in a GuParameter constructor must be of type GuStack",
+      loc: scope.loc,
+    });
+  }
+
+  return null;
+};
+
 module.exports = {
   meta: {
     type: "suggestion",
@@ -46,8 +104,12 @@ module.exports = {
 
   create(context) {
     return {
-      MethodDefinition(node) {
+      MethodDefinition: function (node) {
         if (node.kind !== "constructor") return null;
+
+        if (isGuParameter(node)) {
+          return parseGuParameter(context, node);
+        }
 
         const params = node.value.params;
 
@@ -69,14 +131,14 @@ module.exports = {
           );
         }
 
-        const scope = params[0];
+        const [scope, id, props] = params;
 
         // 3. First parameter must be called scope
         if (scope.name !== "scope") {
           return context.report(
             node,
             scope.loc,
-            `The first parameter in a construct or pattern contructor must be called scope`
+            `The first parameter in a construct or pattern constructor must be called scope`
           );
         }
 
@@ -85,18 +147,16 @@ module.exports = {
           return context.report(
             node,
             scope.typeAnnotation.loc,
-            `The first parameter in a construct or pattern contructor must be of type GuStack`
+            `The first parameter in a construct or pattern constructor must be of type GuStack`
           );
         }
-
-        const id = params[1];
 
         // 5. Second parameter must be called id
         if (getStandardOrLeftProp(id, "name") !== "id") {
           return context.report(
             node,
             id.loc,
-            `The second parameter in a construct or pattern contructor must be called id`
+            `The second parameter in a construct or pattern constructor must be called id`
           );
         }
 
@@ -105,11 +165,9 @@ module.exports = {
           return context.report(
             node,
             getStandardOrLeftProp(id, "typeAnnotation.loc"),
-            `The second parameter in a construct or pattern contructor must be of type string`
+            `The second parameter in a construct or pattern constructor must be of type string`
           );
         }
-
-        const props = params[2];
 
         if (props) {
           // 7. Third parameter (if exists) must called props
@@ -117,7 +175,7 @@ module.exports = {
             return context.report(
               node,
               props.loc,
-              `The third parameter in a construct or pattern contructor must be called props`
+              `The third parameter in a construct or pattern constructor must be called props`
             );
           }
 
@@ -135,7 +193,7 @@ module.exports = {
             return context.report(
               node,
               getStandardOrLeftProp(props, "typeAnnotation.loc"),
-              `The third parameter in a construct or pattern contructor must be a custom type`
+              `The third parameter in a construct or pattern constructor must be a custom type`
             );
           }
         }
