f24717acc4217cb861189791ca8d2a0ce5393cad
Akash Askoolum <akash1810@gmail.com>
2021-03-09 09:46:44 +0000

    fix: provide a default description for GuCertificateArnParameter and allow it to be overridden

--- src/constructs/core/parameters/acm.ts
+++ src/constructs/core/parameters/acm.ts
@@ -6,9 +6,10 @@ import { GuStringParameter } from "./base";
 export class GuCertificateArnParameter extends GuStringParameter {
   constructor(scope: GuStack, id: string = "TLSCertificate", props?: GuNoTypeParameterProps) {
     super(scope, id, {
-      ...props,
       allowedPattern: RegexPattern.ACM_ARN,
       constraintDescription: "Must be an ACM ARN resource",
+      description: "The ARN of an ACM certificate for use on a load balancer",
+      ...props,
     });
   }
 }
