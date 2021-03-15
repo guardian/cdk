d44cf25e4f7a4daf1c9e7b2c564cc1507f332d7b
Jacob Winch <jacob.winch@guardian.co.uk>
2021-03-15 09:41:48 +0000

    docs: Fix documentation for utils directory (#317)

--- typedoc.js
+++ typedoc.js
@@ -1,16 +1,19 @@
 const { readdirSync } = require("fs");
 
 const constructsDir = "src/constructs";
+const utilsDir = "src/utils";
+
+function getEntryPointsFromSubdirectories(directory) {
+  return readdirSync(directory).map((dir) => `${directory}/${dir}/index.ts`);
+}
 
 module.exports = {
   entryPoints: [
     "src/index.ts",
     "src/constants/index.ts",
-    "src/utils/index.ts",
-
-    // we purposefully do not have an index.ts in `src/constructs` to encourage usage of patterns
-    // list sub directories for tsdoc to generate documentation for the constructs
-    ...readdirSync(constructsDir).map((dir) => `${constructsDir}/${dir}/index.ts`),
+    // we purposefully do not have an index.ts in `src/constructs` or `src/utils`
+    ...getEntryPointsFromSubdirectories(constructsDir),
+    ...getEntryPointsFromSubdirectories(utilsDir),
   ],
   out: "target",
   includeVersion: true,
