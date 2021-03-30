62227da6c23843b2e65636523cd7415ba5d6e702
Akash Askoolum <akash1810@gmail.com>
2021-03-30 21:59:48 +0100

    chore: add an integration test project

---
+++ integration-test/jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  testMatch: ["<rootDir>/src/**/*.test.ts"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+};
---
+++ integration-test/src/integration-test-stack.test.ts
@@ -0,0 +1,12 @@
+import "@aws-cdk/assert/jest";
+import { SynthUtils } from "@aws-cdk/assert";
+import { App } from "@aws-cdk/core";
+import { IntegrationTestStack } from "./integration-test-stack";
+
+describe("The Cdk stack", () => {
+  it("matches the snapshot", () => {
+    const app = new App();
+    const stack = new IntegrationTestStack(app, "cdk", { stack: "integration-test" });
+    expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
+  });
+});
