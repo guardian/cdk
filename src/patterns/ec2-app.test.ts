cf81c9c1501b09054f36cad60a5a9b3244bb724d
stephengeller <stephen.geller@hotmail.com>
2021-04-06 17:20:51 +0100

    Create test for load balancer

--- src/patterns/ec2-app.test.ts
+++ src/patterns/ec2-app.test.ts
@@ -1,3 +1,4 @@
+import "@aws-cdk/assert/jest";
 import { SynthUtils } from "@aws-cdk/assert";
 import { simpleGuStackForTesting } from "../utils/test";
 import { GuApplicationPorts, GuEc2App } from "./ec2-app";
@@ -13,5 +14,25 @@ describe("the GuEC2App pattern", function () {
     });
 
     expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
+  });
+
+  it("should contain a load balancer with a listener and target group", function () {
+    const stack = simpleGuStackForTesting();
+    new GuEc2App(stack, {
+      applicationPort: GuApplicationPorts.Node,
+      app: "test-gu-ec2-app",
+      publicFacing: false,
+      userData: "#!/bin/dev foobarbaz",
+    });
+
+    const json = SynthUtils.toCloudFormation(stack) as JSON;
+
+    expect(json).toHaveResource("AWS::ElasticLoadBalancingV2::LoadBalancer", {
+      Subnets: { Ref: "PrivateSubnets" },
+    });
+    expect(json).toHaveResource("AWS::ElasticLoadBalancingV2::Listener", {
+      Certificates: [{ CertificateArn: { Ref: "CertArn" } }],
+    });
+    expect(json).toHaveResource("AWS::ElasticLoadBalancingV2::TargetGroup");
   });
 });
