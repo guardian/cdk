78d66328519797d475b82e93ad9ba4d66f534428
Jacob Winch <jacob.winch@guardian.co.uk>
2021-04-07 16:08:45 +0100

    Tidy up tests now that we have a functional stack

--- src/patterns/ec2-app.test.ts
+++ src/patterns/ec2-app.test.ts
@@ -4,7 +4,7 @@ import { simpleGuStackForTesting } from "../utils/test";
 import { GuApplicationPorts, GuEc2App } from "./ec2-app";
 
 describe("the GuEC2App pattern", function () {
-  it("should compile", function () {
+  it("should produce a functional EC2 app with minimal arguments", function () {
     const stack = simpleGuStackForTesting();
     new GuEc2App(stack, {
       applicationPort: GuApplicationPorts.Node,
@@ -12,27 +12,6 @@ describe("the GuEC2App pattern", function () {
       publicFacing: false,
       userData: "#!/bin/dev foobarbaz",
     });
-
     expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
-  });
-
-  it("should contain a load balancer with a listener and target group", function () {
-    const stack = simpleGuStackForTesting();
-    new GuEc2App(stack, {
-      applicationPort: GuApplicationPorts.Node,
-      app: "test-gu-ec2-app",
-      publicFacing: false,
-      userData: "#!/bin/dev foobarbaz",
-    });
-
-    const json = SynthUtils.toCloudFormation(stack) as JSON;
-
-    expect(json).toHaveResource("AWS::ElasticLoadBalancingV2::LoadBalancer", {
-      Subnets: { Ref: "PrivateSubnets" },
-    });
-    expect(json).toHaveResource("AWS::ElasticLoadBalancingV2::Listener", {
-      Certificates: [{ CertificateArn: { Ref: "CertArn" } }],
-    });
-    expect(json).toHaveResource("AWS::ElasticLoadBalancingV2::TargetGroup");
   });
 });
