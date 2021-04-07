9f9af452a45220a307c16cc1a1a81d631b4c9def
Jacob Winch <jacob.winch@guardian.co.uk>
2021-04-07 08:21:45 +0100

    Make Hosted Zones optional and update tests

--- src/constructs/acm/certificate.test.ts
+++ src/constructs/acm/certificate.test.ts
@@ -4,7 +4,7 @@ import { simpleGuStackForTesting } from "../../utils/test";
 import { GuCertificate } from "./certificate";
 
 describe("The GuCertificate class", () => {
-  it("should create a new certificate from scratch", () => {
+  it("should create a new certificate when hosted zone ids are provided", () => {
     const stack = simpleGuStackForTesting();
     new GuCertificate(stack, "TestCertificate", {
       [Stage.CODE]: {
@@ -14,6 +14,19 @@ describe("The GuCertificate class", () => {
       [Stage.PROD]: {
         domainName: "prod-guardian.com",
         hostedZoneId: "id124",
+      },
+    });
+    expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
+  });
+
+  it("should create a new certificate (which requires manual DNS changes) if hosted zone ids are not provided", () => {
+    const stack = simpleGuStackForTesting();
+    new GuCertificate(stack, "TestCertificate", {
+      [Stage.CODE]: {
+        domainName: "code-guardian.com",
+      },
+      [Stage.PROD]: {
+        domainName: "prod-guardian.com",
       },
     });
     expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
--- src/constructs/acm/certificate.ts
+++ src/constructs/acm/certificate.ts
@@ -9,25 +9,28 @@ type GuCertificateProps = Record<Stage, GuDnsValidatedCertificateProps>;
 
 interface GuDnsValidatedCertificateProps {
   domainName: string;
-  hostedZoneId: string;
+  hostedZoneId?: string;
 }
 
 export class GuCertificate extends Certificate {
   constructor(scope: GuStack, id: string, props: GuCertificateProps) {
-    const hostedZone = HostedZone.fromHostedZoneId(
-      scope,
-      "HostedZone",
-      scope.withStageDependentValue({
-        variableName: "hostedZoneId",
-        stageValues: { [Stage.CODE]: props.CODE.hostedZoneId, [Stage.PROD]: props.PROD.hostedZoneId },
-      })
-    );
+    const maybeHostedZone =
+      props.CODE.hostedZoneId && props.PROD.hostedZoneId
+        ? HostedZone.fromHostedZoneId(
+            scope,
+            "HostedZone",
+            scope.withStageDependentValue({
+              variableName: "hostedZoneId",
+              stageValues: { [Stage.CODE]: props.CODE.hostedZoneId, [Stage.PROD]: props.PROD.hostedZoneId },
+            })
+          )
+        : undefined;
     const awsCertificateProps: CertificateProps = {
       domainName: scope.withStageDependentValue({
         variableName: "domainName",
         stageValues: { [Stage.CODE]: props.CODE.domainName, [Stage.PROD]: props.PROD.domainName },
       }),
-      validation: CertificateValidation.fromDns(hostedZone),
+      validation: CertificateValidation.fromDns(maybeHostedZone),
     };
     super(scope, id, awsCertificateProps);
     this.applyRemovalPolicy(RemovalPolicy.RETAIN);
