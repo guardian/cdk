01e271b6b72ca473cf5bebdfb38a843387b5555f
Jacob Winch <jacob.winch@guardian.co.uk>
2021-04-08 09:31:02 +0100

    fix: ensure all generated logical ids conform to CloudFormation standard (#396)

--- src/constructs/core/identity.ts
+++ src/constructs/core/identity.ts
@@ -15,7 +15,9 @@ export interface Identity extends StackStageIdentity, AppIdentity {}
 export const AppIdentity = {
   suffixText(appIdentity: AppIdentity, text: string): string {
     const titleCaseApp = appIdentity.app.charAt(0).toUpperCase() + appIdentity.app.slice(1);
-    return `${text}${titleCaseApp}`;
+    // CloudFormation Logical Ids must be alphanumeric, so remove any non-alphanumeric characters: https://stackoverflow.com/a/20864946
+    const alphanumericTitleCaseApp = titleCaseApp.replace(/[\W_]+/g, "");
+    return `${text}${alphanumericTitleCaseApp}`;
   },
   taggedConstruct<T extends IConstruct>(appIdentity: AppIdentity, construct: T): T {
     Tags.of(construct).add("App", appIdentity.app);
