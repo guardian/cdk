2951f2b514b992c8d0de482addf50132464f16fd
Akash Askoolum <akash1810@gmail.com>
2021-04-09 14:36:59 +0100

    refactor: Make `findResourceByTypeAndLogicalId` standalone
    The `findResourceByTypeAndLogicalId` function is useful in other contexts than just assertions.
    
    This is partly inspired by the AWS CDK library.
    
    See:
      - https://github.com/aws/aws-cdk/blob/ecc9bf386ca088ca82a332c649f13613b9793628/packages/%40aws-cdk/assert/jest.ts#L63

--- src/utils/test/jest.ts
+++ src/utils/test/jest.ts
@@ -1,6 +1,5 @@
-import { SynthUtils } from "@aws-cdk/assert";
 import type { GuStack } from "../../constructs/core";
-import type { SynthedStack } from "./synthed-stack";
+import { findResourceByTypeAndLogicalId } from "./synthed-stack";
 
 declare global {
   // eslint-disable-next-line @typescript-eslint/no-namespace -- custom Jest matcher
@@ -20,13 +19,7 @@ expect.extend({
    * @param logicalId a string or regex pattern to match against the resource's logicalId
    */
   toHaveResourceOfTypeAndLogicalId(stack: GuStack, resourceType: string, logicalId: string | RegExp) {
-    const json = SynthUtils.toCloudFormation(stack) as SynthedStack;
-
-    const matchResult = Object.entries(json.Resources).find(([key, { Type }]) => {
-      const logicalIdMatch = logicalId instanceof RegExp ? logicalId.test(key) : key === logicalId;
-      const typeMatch = Type === resourceType;
-      return logicalIdMatch && typeMatch;
-    });
+    const matchResult = findResourceByTypeAndLogicalId(stack, resourceType, logicalId);
 
     return matchResult
       ? {
