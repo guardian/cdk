51503781a933b450eb4cd490c60ae05a9a350ce1
Jacob Winch <jacob.winch@guardian.co.uk>
2021-04-13 14:12:24 +0100

    Attempt to add EC2-based alarms

---
+++ src/constructs/cloudwatch/ec2-alarms.test.ts
@@ -0,0 +1,62 @@
+import "@aws-cdk/assert/jest";
+import { SynthUtils } from "@aws-cdk/assert";
+import { Vpc } from "@aws-cdk/aws-ec2";
+import { Stack } from "@aws-cdk/core";
+import { simpleGuStackForTesting } from "../../utils/test";
+import type { AppIdentity } from "../core/identity";
+import { GuApplicationLoadBalancer } from "../loadbalancing";
+import { Gu5xxPercentageAlarm } from "./ec2-alarms";
+
+const vpc = Vpc.fromVpcAttributes(new Stack(), "VPC", {
+  vpcId: "test",
+  availabilityZones: [""],
+  publicSubnetIds: [""],
+});
+
+const app: AppIdentity = {
+  app: "testing",
+};
+
+describe("The Gu5xxPercentageAlarm construct", () => {
+  it("should create the correct alarm resource with minimal config", () => {
+    const stack = simpleGuStackForTesting();
+    const alb = new GuApplicationLoadBalancer(stack, "ApplicationLoadBalancer", { ...app, vpc });
+    const props = {
+      tolerated5xxPercentage: 1,
+      snsTopicName: "test-topic",
+      evaluationPeriods: 1, //FIXME
+    };
+    new Gu5xxPercentageAlarm(stack, "test", { ...app, loadBalancer: alb, ...props });
+    expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
+  });
+
+  it("should use a custom description if one is provided", () => {
+    const stack = simpleGuStackForTesting();
+    const alb = new GuApplicationLoadBalancer(stack, "ApplicationLoadBalancer", { ...app, vpc });
+    const props = {
+      alarmDescription: "test-custom-alarm-description",
+      tolerated5xxPercentage: 1,
+      snsTopicName: "test-topic",
+      evaluationPeriods: 1, //FIXME
+    };
+    new Gu5xxPercentageAlarm(stack, "test", { ...app, loadBalancer: alb, ...props });
+    expect(stack).toHaveResource("AWS::CloudWatch::Alarm", {
+      AlarmDescription: "test-custom-alarm-description",
+    });
+  });
+
+  it("should use a custom alarm name if one is provided", () => {
+    const stack = simpleGuStackForTesting();
+    const alb = new GuApplicationLoadBalancer(stack, "ApplicationLoadBalancer", { ...app, vpc });
+    const props = {
+      alarmName: "test-custom-alarm-name",
+      tolerated5xxPercentage: 1,
+      snsTopicName: "test-topic",
+      evaluationPeriods: 1, //FIXME
+    };
+    new Gu5xxPercentageAlarm(stack, "test", { ...app, loadBalancer: alb, ...props });
+    expect(stack).toHaveResource("AWS::CloudWatch::Alarm", {
+      AlarmName: "test-custom-alarm-name",
+    });
+  });
+});
---
+++ src/constructs/cloudwatch/ec2-alarms.ts
@@ -0,0 +1,49 @@
+import { ComparisonOperator, MathExpression, TreatMissingData } from "@aws-cdk/aws-cloudwatch";
+import { HttpCodeElb, HttpCodeTarget } from "@aws-cdk/aws-elasticloadbalancingv2";
+import type { GuStack } from "../core";
+import type { AppIdentity } from "../core/identity";
+import type { GuApplicationLoadBalancer } from "../loadbalancing";
+import type { GuAlarmProps } from "./alarm";
+import { GuAlarm } from "./alarm";
+
+/**
+ * Creates an alarm which is triggered whenever the percentage of requests with a 5xx response code exceeds
+ * the specified threshold.
+ */
+export interface Gu5xxPercentageMonitoringProps
+  extends Omit<GuAlarmProps, "metric" | "threshold" | "treatMissingData">,
+    AppIdentity {
+  tolerated5xxPercentage: number;
+  noMonitoring?: false;
+}
+
+interface GuLoadBalancerAlarmProps extends Gu5xxPercentageMonitoringProps {
+  loadBalancer: GuApplicationLoadBalancer;
+}
+
+// TODO - how should we handle evaluation periods
+export class Gu5xxPercentageAlarm extends GuAlarm {
+  constructor(scope: GuStack, id: string, props: GuLoadBalancerAlarmProps) {
+    const mathExpression = new MathExpression({
+      expression: "100*(m1+m2)/m3",
+      usingMetrics: {
+        m1: props.loadBalancer.metricHttpCodeElb(HttpCodeElb.ELB_5XX_COUNT),
+        m2: props.loadBalancer.metricHttpCodeTarget(HttpCodeTarget.TARGET_5XX_COUNT),
+        m3: props.loadBalancer.metricRequestCount(),
+      },
+      label: `% of 5XX responses served for ${props.app} (load balancer and instances combined)`,
+    });
+    const defaultAlarmName = `High 5XX error % from ${props.app} in ${scope.stage}`;
+    const defaultDescription = `${props.app} exceeded ${props.tolerated5xxPercentage}% error rate`;
+    const alarmProps = {
+      ...props,
+      metric: mathExpression,
+      treatMissingData: TreatMissingData.NOT_BREACHING,
+      threshold: props.tolerated5xxPercentage,
+      comparisonOperator: ComparisonOperator.GREATER_THAN_THRESHOLD,
+      alarmName: props.alarmName ?? defaultAlarmName,
+      alarmDescription: props.alarmDescription ?? defaultDescription,
+    };
+    super(scope, id, alarmProps);
+  }
+}
