fc1d6ef6be923b8592f4ec97d2be3f9bc99e0a9f
Akash Askoolum <akash1810@gmail.com>
2021-04-21 11:25:32 +0100

    chore(docs): Add docs for `GuApiLambda`

--- src/patterns/api-lambda.ts
+++ src/patterns/api-lambda.ts
@@ -10,10 +10,35 @@ interface ApiProps extends Omit<LambdaRestApiProps, "handler"> {
 }
 
 interface GuApiLambdaProps extends Omit<GuFunctionProps, "errorPercentageMonitoring"> {
+  /**
+   * A list of [[`LambdaRestApiProps`]] to configure for the lambda.
+   */
   apis: ApiProps[];
+
+  /**
+   * Configuration for an alarm based on the lambda's error percentage.
+   *
+   * For example:
+   *
+   * ```typescript
+   * monitoringConfiguration: {
+   *   toleratedErrorPercentage: <sensible_error_percentage_threshold>,
+   *   snsTopicName: "my-topic-for-cloudwatch-alerts",
+   * }
+   * ```
+   *
+   * If your team do not use CloudWatch, it's possible to opt-out with the following configuration:
+   * ```typescript
+   *  monitoringConfiguration: { noMonitoring: true }
+   * ```
+   */
   monitoringConfiguration: NoMonitoring | GuLambdaErrorPercentageMonitoringProps;
 }
 
+/**
+ * A pattern to create a lambda triggered by API Gateway
+ * @see https://docs.aws.amazon.com/apigateway/latest/developerguide/getting-started-with-lambda-integration.html
+ */
 export class GuApiLambda extends GuLambdaFunction {
   constructor(scope: GuStack, id: string, props: GuApiLambdaProps) {
     super(scope, id, {
