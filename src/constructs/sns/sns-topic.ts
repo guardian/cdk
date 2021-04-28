c635841f5c8e35274809bf2dfe07b4e342ca1a1b
Jacob Winch <jacob.winch@guardian.co.uk>
2021-04-28 12:40:45 +0100

    docs: Add basic docs for Kinesis streams and SNS topics

--- src/constructs/sns/sns-topic.ts
+++ src/constructs/sns/sns-topic.ts
@@ -6,6 +6,22 @@ import type { GuMigratingResource } from "../core/migrating";
 
 interface GuSnsTopicProps extends TopicProps, GuMigratingResource {}
 
+/**
+ * Construct which creates an SNS Topic.
+ *
+ * To inherit an SNS topic which has already been created via a CloudFormation stack, the `migratedFromCloudFormation`
+ * prop on your stack must be set to `true`. You should also pass in the logical id via the `existingLogicalId` prop.
+ *
+ * For example, when migrating a CloudFormation stack which includes the following resource:
+ * ```yaml
+ * MyCloudFormedSnsTopic:
+ *   Type: AWS::SNS::Topic
+ * ```
+ * Inherit the SNS topic (rather than creating a new one) using:
+ * ```typescript
+ *  new GuSnsTopic(stack, "SnsTopic", { existingLogicalId: "MyCloudFormedSnsTopic" });
+ * ```
+ */
 export class GuSnsTopic extends GuStatefulMigratableConstruct(Topic) {
   constructor(scope: GuStack, id: string, props?: GuSnsTopicProps) {
     super(scope, id, props);
