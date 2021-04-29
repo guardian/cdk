53b92ab670eea219433c7c0766b91a6b796d0be6
Akash Askoolum <akash1810@gmail.com>
2021-04-29 08:21:34 +0100

    chore: Demonstrate singleton usage
    Adds the `GuDistributionBucketParameter` singleton construct to the
    integration tests to demonstrate its usage across multiple `Stack`s in
    the same `App` instance.

--- integration-test/src/integration-test-stack.ts
+++ integration-test/src/integration-test-stack.ts
@@ -1,9 +1,10 @@
 import type { App } from "@aws-cdk/core";
 import type { GuStackProps } from "@guardian/cdk/lib/constructs/core";
-import { GuStack } from "@guardian/cdk/lib/constructs/core";
+import { GuDistributionBucketParameter, GuStack } from "@guardian/cdk/lib/constructs/core";
 
 export class IntegrationTestStack extends GuStack {
   constructor(scope: App, id: string, props: GuStackProps) {
     super(scope, id, props);
+    GuDistributionBucketParameter.getInstance(this);
   }
 }
