8b7b74000182360d8f88c703148a7f6e29fba1c5
Jacob Winch <jacob.winch@guardian.co.uk>
2021-05-18 12:42:31 +0100

    chore: fix order of imports in integration test

--- integration-test/src/integration-test-stack.ts
+++ integration-test/src/integration-test-stack.ts
@@ -1,6 +1,6 @@
+import { GuDistributionBucketParameter, GuStack } from "@guardian/cdk/lib/constructs/core";
 import type { App } from "@aws-cdk/core";
 import type { GuStackProps } from "@guardian/cdk/lib/constructs/core";
-import { GuDistributionBucketParameter, GuStack } from "@guardian/cdk/lib/constructs/core";
 
 export class IntegrationTestStack extends GuStack {
   constructor(scope: App, id: string, props: GuStackProps) {
