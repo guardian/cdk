eed6bfab79d62406b7ee08eee56eddbdf41d6987
Akash Askoolum <akash1810@gmail.com>
2021-06-29 19:22:28 +0100

    feat!: Make `GuStageParameter` a singleton
    Converts the `GuStageParameter` construct into a singleton to simplify
    references to it across constructs.
    
    Before, we had to something like:
    
    ```typescript
    const stage = this.getParam<GuStageParameter>("Stage");
    ```
    
    With this change, this becomes:
    
    ```typescript
    GuStageParameter.getInstance(this).valueAsString
    ```
    
    BREAKING CHANGE: `GuStageParameter` is now a singleton
    
    Making `GuStageParameter` a singleton simplifies access to it.
    Note, `this.stage` on the `GuStack` is still possible.

--- src/constructs/core/parameters/identity.ts
+++ src/constructs/core/parameters/identity.ts
@@ -1,13 +1,24 @@
 import { Stage, Stages } from "../../../constants";
+import { isSingletonPresentInStack } from "../../../utils/test";
 import type { GuStack } from "../stack";
 import { GuStringParameter } from "./base";
 
 export class GuStageParameter extends GuStringParameter {
-  constructor(scope: GuStack) {
+  private static instance: GuStageParameter | undefined;
+
+  private constructor(scope: GuStack) {
     super(scope, "Stage", {
       description: "Stage name",
       allowedValues: Stages,
       default: Stage.CODE,
     });
+  }
+
+  public static getInstance(stack: GuStack): GuStageParameter {
+    if (!this.instance || !isSingletonPresentInStack(stack, this.instance)) {
+      this.instance = new GuStageParameter(stack);
+    }
+
+    return this.instance;
   }
 }
