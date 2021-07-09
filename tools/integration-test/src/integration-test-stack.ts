dd5c79a9d35e12347b78dd1051bfd1f8844b34b5
Akash Askoolum <akash1810@gmail.com>
2021-07-09 16:09:31 +0100

    chore: Testing `GuVpcParameter` for Phil

--- tools/integration-test/src/integration-test-stack.ts
+++ tools/integration-test/src/integration-test-stack.ts
@@ -1,10 +1,13 @@
 import type { App } from "@aws-cdk/core";
-import { GuDistributionBucketParameter, GuStack } from "@guardian/cdk/lib/constructs/core";
 import type { GuStackProps } from "@guardian/cdk/lib/constructs/core";
+import { GuDistributionBucketParameter, GuStack, GuVpcParameter } from "@guardian/cdk/lib/constructs/core";
+import { GuVpc } from "@guardian/cdk/lib/constructs/ec2";
 
 export class IntegrationTestStack extends GuStack {
   constructor(scope: App, id: string, props: GuStackProps) {
     super(scope, id, props);
     GuDistributionBucketParameter.getInstance(this);
+
+    GuVpc.fromId(this, "vpc", { vpcId: GuVpcParameter.getInstance(this).valueAsString });
   }
 }
