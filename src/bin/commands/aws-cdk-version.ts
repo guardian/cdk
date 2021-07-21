0dd4811c6b57f8e2ac0284701ed85108fd1e7947
Samantha Gottlieb <samanthagottlieb31@gmail.com>
2021-07-21 15:05:27 +0100

    feat: Make cli verbose by default

--- src/bin/commands/aws-cdk-version.ts
+++ src/bin/commands/aws-cdk-version.ts
@@ -1,6 +1,6 @@
 import { LibraryInfo } from "../../constants/library-info";
 import type { CliCommandResponse } from "../../types/command";
 
-export const awsCdkVersionCommand = (verbose: boolean): CliCommandResponse => {
-  return Promise.resolve(verbose ? LibraryInfo.AWS_CDK_VERSIONS : LibraryInfo.AWS_CDK_VERSION);
+export const awsCdkVersionCommand = (): CliCommandResponse => {
+  return Promise.resolve(LibraryInfo.AWS_CDK_VERSIONS);
 };
