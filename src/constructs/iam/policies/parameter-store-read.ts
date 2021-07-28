0279ee7432f85ecefb7e9128439d18a5db79caf1
Simon Hildrew <simon@hildrew.net>
2021-07-28 17:11:46 +0100

    feat: add SSM read to Lambda construct for app params
    This adds a policy statement to the Lambda construct so that permission is always granted to read SSM parameters under  $stage/$stack/$app/* for a lambda

--- src/constructs/iam/policies/parameter-store-read.ts
+++ src/constructs/iam/policies/parameter-store-read.ts
@@ -3,19 +3,21 @@ import type { GuStack } from "../../core";
 import { AppIdentity } from "../../core/identity";
 import { GuPolicy } from "./base-policy";
 
+export class GuParameterStoreReadPolicyStatement extends PolicyStatement {
+  constructor(scope: GuStack, props: AppIdentity) {
+    super({
+      effect: Effect.ALLOW,
+      actions: ["ssm:GetParametersByPath"],
+      resources: [`arn:aws:ssm:${scope.region}:${scope.account}:parameter/${scope.stage}/${scope.stack}/${props.app}`],
+    });
+  }
+}
+
 export class GuParameterStoreReadPolicy extends GuPolicy {
   constructor(scope: GuStack, props: AppIdentity) {
     super(scope, AppIdentity.suffixText(props, "ParameterStoreRead"), {
       policyName: "parameter-store-read-policy",
-      statements: [
-        new PolicyStatement({
-          effect: Effect.ALLOW,
-          actions: ["ssm:GetParametersByPath"],
-          resources: [
-            `arn:aws:ssm:${scope.region}:${scope.account}:parameter/${scope.stage}/${scope.stack}/${props.app}`,
-          ],
-        }),
-      ],
+      statements: [new GuParameterStoreReadPolicyStatement(scope, props)],
     });
 
     AppIdentity.taggedConstruct(props, this);
