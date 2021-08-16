fde09b3e03b27def41ceb72f4486f969269bcb92
Stephen Geller <stephen.gb.geller+git@gmail.com>
2021-08-16 14:08:57 +0100

    feat(cli): improve account-readiness cli output
    This commit ensures the output of the account-readiness cli tool is more legible by the user

--- src/bin/commands/account-readiness.ts
+++ src/bin/commands/account-readiness.ts
@@ -1,5 +1,6 @@
 import type { CredentialProviderChain } from "aws-sdk";
 import AWS from "aws-sdk";
+import chalk from "chalk";
 import { SSM_PARAMETER_PATHS } from "../../constants/ssm-parameter-paths";
 import type { CliCommandResponse } from "../../types/command";
 
@@ -24,5 +25,35 @@ export const accountReadinessCommand = async ({
 
   const notFoundInDetail = ssmParams.filter((_) => notFoundParameters.includes(_.path));
   const foundInDetail = ssmParams.filter((_) => !notFoundParameters.includes(_.path));
-  return Promise.resolve({ found: foundInDetail, notFound: notFoundInDetail });
+
+  if (foundInDetail.length > 0) {
+    console.log(chalk.bold("Existing required parameters:"));
+    console.log(
+      foundInDetail
+        .map((param) => {
+          const dimDescription = chalk.dim(`(${param.description})`);
+          return ` ✅ ${chalk.yellowBright(param.path)} ${dimDescription}`;
+        })
+        .join("\n"),
+      "\n"
+    );
+  }
+
+  if (notFoundInDetail.length <= 0) {
+    return chalk.green("AWS account contains all required parameters!");
+  } else {
+    console.log(chalk.bold("Missing required parameters:"));
+    console.log(
+      notFoundInDetail
+        .map((param) => {
+          const dimDescription = chalk.dim(`(${param.description})`);
+          return ` ❌ ${chalk.yellowBright(param.path)} ${dimDescription}`;
+        })
+        .join("\n"),
+      "\n"
+    );
+    return chalk.red(
+      `AWS account requires the above ${notFoundInDetail.length} required parameters to work with @guardian/cdk`
+    );
+  }
 };
