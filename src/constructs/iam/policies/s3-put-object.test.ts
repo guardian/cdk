01c3e03049d9cc749925ba8e106eaaad005a8c31
Francesca Hammond <francesca.hammond@guardian.co.uk>
2021-08-20 16:22:17 +0100

    Create s3-put-object test

--- src/constructs/iam/policies/s3-put-object.test.ts
+++ src/constructs/iam/policies/s3-put-object.test.ts
@@ -1,15 +1,12 @@
 import "@aws-cdk/assert/jest";
-import { SynthUtils } from "@aws-cdk/assert/lib/synth-utils";
 import { attachPolicyToTestRole, simpleGuStackForTesting } from "../../../utils/test";
-import type { SynthedStack } from "../../../utils/test";
-import { GuDistributionBucketParameter } from "../../core";
-import { GuGetDistributablePolicy, GuGetS3ObjectsPolicy } from "./s3-get-object";
+import { GuPutS3ObjectsPolicy } from "./s3-put-object";
 
-describe("The GuGetS3ObjectPolicy class", () => {
+describe("The GuPutS3ObjectPolicy class", () => {
   it("sets default props", () => {
     const stack = simpleGuStackForTesting();
 
-    const s3Policy = new GuGetS3ObjectsPolicy(stack, "S3Policy", { bucketName: "test" });
+    const s3Policy = new GuPutS3ObjectsPolicy(stack, "S3Policy", { bucketName: "test" });
 
     attachPolicyToTestRole(stack, s3Policy);
 
@@ -20,7 +17,7 @@ describe("The GuGetS3ObjectPolicy class", () => {
           {
             Effect: "Allow",
             Resource: "arn:aws:s3:::test/*",
-            Action: "s3:GetObject",
+            Action: "s3:PutObject",
           },
         ],
       },
@@ -30,7 +27,7 @@ describe("The GuGetS3ObjectPolicy class", () => {
   it("merges defaults and passed in props", () => {
     const stack = simpleGuStackForTesting();
 
-    const s3Policy = new GuGetS3ObjectsPolicy(stack, "S3Policy", { bucketName: "test", policyName: "test" });
+    const s3Policy = new GuPutS3ObjectsPolicy(stack, "S3Policy", { bucketName: "test", policyName: "test" });
 
     attachPolicyToTestRole(stack, s3Policy);
 
@@ -42,7 +39,7 @@ describe("The GuGetS3ObjectPolicy class", () => {
           {
             Effect: "Allow",
             Resource: "arn:aws:s3:::test/*",
-            Action: "s3:GetObject",
+            Action: "s3:PutObject",
           },
         ],
       },
@@ -52,7 +49,7 @@ describe("The GuGetS3ObjectPolicy class", () => {
   it("handles multiple paths correctly", () => {
     const stack = simpleGuStackForTesting();
 
-    const s3Policy = new GuGetS3ObjectsPolicy(stack, "S3Policy", {
+    const s3Policy = new GuPutS3ObjectsPolicy(stack, "S3Policy", {
       bucketName: "test",
       paths: ["file1.txt", "file2.txt"],
     });
@@ -66,54 +63,7 @@ describe("The GuGetS3ObjectPolicy class", () => {
           {
             Effect: "Allow",
             Resource: ["arn:aws:s3:::test/file1.txt", "arn:aws:s3:::test/file2.txt"],
-            Action: "s3:GetObject",
-          },
-        ],
-      },
-    });
-  });
-});
-
-describe("The GuGetDistributablePolicy construct", () => {
-  it("creates the correct policy", () => {
-    const stack = simpleGuStackForTesting();
-    attachPolicyToTestRole(stack, new GuGetDistributablePolicy(stack, { app: "testing" }));
-
-    const json = SynthUtils.toCloudFormation(stack) as SynthedStack;
-
-    const parameterKeys = Object.keys(json.Parameters);
-    const expectedKeys = ["Stage", GuDistributionBucketParameter.getInstance(stack).id];
-    expect(parameterKeys).toEqual(expectedKeys);
-
-    expect(json.Parameters.DistributionBucketName).toEqual({
-      Default: "/account/services/artifact.bucket",
-      Description: "SSM parameter containing the S3 bucket name holding distribution artifacts",
-      Type: "AWS::SSM::Parameter::Value<String>",
-    });
-
-    expect(stack).toHaveResource("AWS::IAM::Policy", {
-      PolicyDocument: {
-        Version: "2012-10-17",
-        Statement: [
-          {
-            Action: "s3:GetObject",
-            Effect: "Allow",
-            Resource: {
-              "Fn::Join": [
-                "",
-                [
-                  "arn:aws:s3:::",
-                  {
-                    Ref: "DistributionBucketName",
-                  },
-                  `/test-stack/`,
-                  {
-                    Ref: "Stage",
-                  },
-                  "/testing/*",
-                ],
-              ],
-            },
+            Action: "s3:PutObject",
           },
         ],
       },
