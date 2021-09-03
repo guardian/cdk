81ecd0c0cbb61f2d85176e84b130bfccb6b1234c
Jon Soul <jonathan.soul@guardian.co.uk>
2021-09-03 13:31:24 +0100

    Docs

--- src/constructs/iam/fastly-logs-iam.ts
+++ src/constructs/iam/fastly-logs-iam.ts
@@ -13,18 +13,45 @@ interface GuFastlyLogsIamProps {
 const FASTLY_AWS_ACCOUNT_ID = "717331877981";
 
 /*
-TODO: docs
+ * Construct which creates the required IAM resources to support Fastly logging to an S3 bucket.
+ * Importantly, it does not create a permanent IAM user, which was once a requirement.
+ *
+ * Specifically, this construct creates a role which can only be assumed in Fastly's AWS account,
+ * with an attached policy allowing PUT access to an S3 bucket and path specified in the props.
+ *
+ * As mentioned, you should configure the S3 bucket used with `bucketName` and the path within
+ * that bucket with the optional `path`. `path` defaults to `"*"` i.e. access is given to the
+ * entire bucket.
+ *
+ * ```typescript
+ * new GuFastlyLogsIam(stack, "FastlyS3Logging", {
+ *   bucketName: "gu-mobile-logs"
+ *   path: "fastly/*"
+ * })
+ * ```
+ *
+ * You may instead wish to create separate roles, rather than a single one.
+ *
+ * ```typescript
+ * new GuFastlyLogsIam(stack, "FastlyS3LoggingMyApp", {
+ *   bucketName: "gu-mobile-logs"
+ *   path: "fastly/my-app/PROD/*"
+ * })
+ * ```
+ *
+ * See https://docs.fastly.com/en/guides/creating-an-aws-iam-role-for-fastly-logging
+ *
  */
 export class GuFastlyLogsIam {
   constructor(scope: GuStack, id: string, props: GuFastlyLogsIamProps) {
-    const policy = new GuPutS3ObjectsPolicy(scope, `${id}FastlyLogsIamPolicy`, {
+    const policy = new GuPutS3ObjectsPolicy(scope, `${id}IamPolicy`, {
       bucketName: props.bucketName,
       paths: props.path ? [props.path] : undefined,
     });
 
     const fastlyCustomerId = GuFastlyCustomerIdParameter.getInstance(scope).valueAsString;
 
-    const role = new GuRole(scope, `${id}FastlyLogsIamRole`, {
+    const role = new GuRole(scope, `${id}IamRole`, {
       assumedBy: new AccountPrincipal(FASTLY_AWS_ACCOUNT_ID),
       externalIds: [fastlyCustomerId],
     });
