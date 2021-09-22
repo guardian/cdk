a0d99c2f7a565fc99ea20a58108d9f495eabd2cc
Jacob Winch <jacob.winch@guardian.co.uk>
2021-09-22 18:09:58 +0100

    chore: add docs for DNS constructs

--- src/constructs/dns/dns-records.ts
+++ src/constructs/dns/dns-records.ts
@@ -12,8 +12,13 @@ export interface GuDnsRecordSetProps {
   ttl?: number;
 }
 
+/**
+ * Do not use this construct directly. Use [[`GuCname`]] instead in order to reduce boilerplate.
+ */
 export class GuDnsRecordSet {
   constructor(scope: GuStack, id: string, props: GuDnsRecordSetProps) {
+    // The spec for this private resource type can be found here:
+    // https://github.com/guardian/cfn-private-resource-types/tree/main/dns/guardian-dns-record-set-type/docs#syntax
     new CfnResource(scope, id, {
       type: "Guardian::DNS::RecordSet",
       properties: {
@@ -28,11 +33,19 @@ export class GuDnsRecordSet {
 }
 
 export interface GuCnameProps {
+  /** The name of the record, for example: xyz.example.com */
   name: string;
+  /** The record your CNAME should point to, for example your Load Balancer DNS name */
   resourceRecord: string;
+  /** The time to live in seconds for the DNS record - this will be set to 3600 seconds (1 hour) by default */
   ttl?: number;
 }
 
+/**
+ * Construct for creating CNAME records in NS1.
+ *
+ * See [[`GuCnameProps`]] for configuration options.
+ */
 export class GuCname extends GuDnsRecordSet {
   constructor(scope: GuStack, id: string, props: GuCnameProps) {
     super(scope, id, {
