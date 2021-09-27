ee70378b23306a48944f8a7debee53ebf1470ef7
Akash Askoolum <akash1810@gmail.com>
2021-09-27 12:49:14 +0100

    feat: Create a construct to create IAM resources for GitHub Actions
    With https://github.com/guardian/actions-assume-aws-role we're able to Assume Role within GitHub Actions.
    
    This change introduces a construct to simplify the creation of this role.
    
    The `GuGithubActionsRole` construct creates the three required resources:
      - `AWS::IAM::Policy`
      - `AWS::IAM::Role`
      - `AWS::IAM::OIDCProvider`
    
    This allows teams to do:
    
    ```typescript
    new GuGithubActionsRole(stack, {
      policies: [
        new GuGetS3ObjectsPolicy(stack, "GetObjects", {
          bucketName: "super-secret-stuff",
        }),
      ],
    });
    ```
    
    Which is smaller than the YAML alternative:
    
    ```yaml
    GitHubBucketAccessPolicy:
      Type: AWS::IAM::Policy
      Properties:
        PolicyName: GitHubBucketAccessPolicy
        PolicyDocument:
          Statement:
            Action:
              - s3:GetObject
            Effect: Allow
            Resource:
              - arn:aws:s3:::super-secret-stuff/*
        Roles:
          - Ref: GitHubRole
    
    GitHubRole:
      Type: AWS::IAM::Role
      Properties:
        AssumeRolePolicyDocument:
          Statement:
            - Effect: Allow
              Action: sts:AssumeRoleWithWebIdentity
              Principal:
                Federated: !Ref GitHubOidc
              Condition:
                StringLike:
                  vstoken.actions.githubusercontent.com:sub: repo:guardian/*
    
    GitHubOidc:
      Type: AWS::IAM::OIDCProvider
      Properties:
        Url: https://vstoken.actions.githubusercontent.com
        ClientIdList: [sigstore]
        ThumbprintList: [a031c46782e6e6c662c2c87c76da9aa62ccabd8e]
    ```

---
+++ src/constructs/iam/roles/github-actions.test.ts
@@ -0,0 +1,50 @@
+import "@aws-cdk/assert/jest";
+import { SynthUtils } from "@aws-cdk/assert";
+import { simpleGuStackForTesting } from "../../../utils/test";
+import { GuGetS3ObjectsPolicy } from "../policies";
+import { GuGithubActionsRole } from "./github-actions";
+
+describe("The GitHubActionsRole construct", () => {
+  it("should create the correct resources with minimal config", () => {
+    const stack = simpleGuStackForTesting();
+    new GuGithubActionsRole(stack, {
+      policies: [
+        new GuGetS3ObjectsPolicy(stack, "GetObjects", {
+          bucketName: "super-secret-stuff",
+        }),
+      ],
+    });
+
+    expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
+  });
+
+  it("should be possible to limit which repositories can assume the role", () => {
+    const stack = simpleGuStackForTesting();
+    new GuGithubActionsRole(stack, {
+      policies: [
+        new GuGetS3ObjectsPolicy(stack, "GetObjects", {
+          bucketName: "super-secret-stuff",
+        }),
+      ],
+      condition: {
+        githubOrganisation: "guardian",
+        repositories: "platform-*",
+      },
+    });
+
+    expect(stack).toHaveResourceLike("AWS::IAM::Role", {
+      AssumeRolePolicyDocument: {
+        Statement: [
+          {
+            Action: "sts:AssumeRoleWithWebIdentity",
+            Condition: {
+              StringLike: {
+                "vstoken.actions.githubusercontent.com:sub": "repo:guardian/platform-*",
+              },
+            },
+          },
+        ],
+      },
+    });
+  });
+});
