11eeb3bffbf7ff76b049cff95abd5ec2c592d092
Nicolas Long <nicolas.long@theguardian.com>
2021-10-27 10:21:58 +0100

    feat: Add infra stage

--- src/constants/stage.ts
+++ src/constants/stage.ts
@@ -1,6 +1,7 @@
 enum Stage {
   CODE = "CODE",
   PROD = "PROD",
+  INFRA = "INFRA", // Use for cross-stage infrastructure, such as VPCs.
 }
 
 // for use in the `allowed values` property of a cloudformation parameter
