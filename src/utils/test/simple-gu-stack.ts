367857bc2a6faff4b7a21a03852fdcdfb0dae1ba
Akash Askoolum <akash1810@gmail.com>
2021-12-03 07:55:26 +0000

    refactor: Extract test helper for infra stacks

--- src/utils/test/simple-gu-stack.ts
+++ src/utils/test/simple-gu-stack.ts
@@ -1,6 +1,6 @@
 import { App } from "@aws-cdk/core";
 import type { Environment } from "@aws-cdk/core";
-import { GuStack } from "../../constructs/core";
+import { GuStack, GuStackForInfrastructure } from "../../constructs/core";
 import type { GuStackProps } from "../../constructs/core";
 
 // Some stacks (such as ones using access logging on load balancers) require specifying a region
@@ -13,3 +13,9 @@ export const simpleGuStackForTesting: (props?: SimpleGuStackProps) => GuStack = 
     stack: props?.stack ?? "test-stack",
     ...props,
   });
+
+export const simpleInfraStackForTesting: () => GuStackForInfrastructure = () => {
+  return new GuStackForInfrastructure(new App(), "Test", {
+    stack: "test-stack",
+  });
+};
