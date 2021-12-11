64c2a36d19d3041119a3d3db2936c9c81ec702da
Akash Askoolum <akash1810@gmail.com>
2021-12-11 09:31:35 +0000

    feat: Add Stack Set support
    Create constructs to support the creation of `AWS::CloudFormation::StackSet` resources.
    
    Usage:
    ```typescript
    // the infrastructure to create in target accounts
    class AccountAlertTopic extends GuStackForStackSetInstance {
      constructor(id: string, props: GuStackProps) {
        super(id, props);
    
        new GuSnsTopic(this, "topic-for-alerts");
      }
    }
    
    class ParentStack extends GuStackForInfrastructure {
      constructor(scope: App, id: string, props: GuStackProps) {
        super(scope, id, props);
    
        new GuStackSet(this, `${id}StackSet`, {
          stackSetInstance: new AccountAlertTopic("Alerts", { stack: props.stack }),
          name: "centralised-alarms",
          description: "Provisioning of standard account alerting resources",
          organisationUnitTargets: [ "o-abcde12345" ]
        });
      }
    }
    
    // contents of `bin/cdk.ts`
    new ParentStack(new App(), "AccountAlarmResources", { stack: "alarms" })
    ```
    
    This will produce a CloudFormation template like this:
    ```yaml
    Resources:
      AccountAlarmResourcesStackSet:
        Type: AWS::CloudFormation::StackSet
        Properties:
          PermissionModel: SERVICE_MANAGED
          StackSetName: centralised-alarms
          AutoDeployment:
            Enabled: true
            RetainStacksOnAccountRemoval: false
          Description: Provisioning of standard account alerting resources
          Parameters: []
          StackInstancesGroup:
            - DeploymentTargets:
                OrganizationalUnitIds:
                  - o-abcde12345
              Regions:
                - Ref: AWS::Region
          Tags:
            - Key: gu:cdk:version
              Value: TEST
            - Key: gu:repo
              Value: guardian/cdk
            - Key: Stack
              Value: alarms
            - Key: Stage
              Value: INFRA
          TemplateBody: |-
            {
              "Resources": {
                "topicforalerts57330FBE": {
                  "Type": "AWS::SNS::Topic",
                  "Properties": {
                    "Tags": [
                      {
                        "Key": "gu:cdk:version",
                        "Value": "TEST"
                      },
                      {
                        "Key": "gu:repo",
                        "Value": "guardian/cdk"
                      },
                      {
                        "Key": "Stack",
                        "Value": "alarms"
                      },
                      {
                        "Key": "Stage",
                        "Value": "INFRA"
                      }
                    ]
                  }
                }
              }
            }
    ```
    
    See https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-cloudformation-stackset.html.

---
+++ src/constructs/stack-set/stack-set.test.ts
@@ -0,0 +1,70 @@
+import { SynthUtils } from "@aws-cdk/assert";
+import { OrganizationPrincipal } from "@aws-cdk/aws-iam";
+import { Topic } from "@aws-cdk/aws-sns";
+import { App } from "@aws-cdk/core";
+import { RegexPattern } from "../../constants";
+import { GuStackForInfrastructure, GuStackForStackSetInstance, GuStringParameter } from "../core";
+import { GuKinesisStream } from "../kinesis";
+import { GuSnsTopic } from "../sns";
+import { GuStackSet } from "./stack-set";
+
+describe("The GuStackSet construct", () => {
+  it("should correctly provision a stack with a stack set resource", () => {
+    const theStackSetInstance = new GuStackForStackSetInstance("the-stack-set", { stack: "test" });
+    new GuKinesisStream(theStackSetInstance, "account-logging-stream");
+
+    const parentStack = new GuStackForInfrastructure(new App(), "Test", { stack: "test" });
+
+    new GuStackSet(parentStack, "StackSet", {
+      name: "my-stack-set",
+      description: "this stack set provisions some common infrastructure",
+      organisationUnitTargets: ["o-12345abcde"],
+      stackSetInstance: theStackSetInstance,
+    });
+
+    expect(SynthUtils.toCloudFormation(parentStack)).toMatchSnapshot();
+  });
+
+  it("should support parameters in the stack set instance", () => {
+    const theStackSetInstance = new GuStackForStackSetInstance("the-stack-set", { stack: "test" });
+    Topic.fromTopicArn(
+      theStackSetInstance,
+      "central-topic",
+      new GuStringParameter(theStackSetInstance, "CentralSnsTopicArn", { allowedPattern: RegexPattern.ARN })
+        .valueAsString
+    );
+
+    const awsOrgId = "o-12345abcde";
+    const parentStack = new GuStackForInfrastructure(new App(), "Test", { stack: "test" });
+    const centralTopic = new GuSnsTopic(parentStack, "account-alerts");
+    centralTopic.grantPublish(new OrganizationPrincipal(awsOrgId));
+
+    new GuStackSet(parentStack, "StackSet", {
+      name: "my-stack-set",
+      description: "this stack set provisions some common infrastructure",
+      organisationUnitTargets: [awsOrgId],
+      stackSetInstance: theStackSetInstance,
+      stackSetInstanceParameters: {
+        CentralSnsTopicArn: centralTopic.topicArn,
+      },
+    });
+
+    expect(SynthUtils.toCloudFormation(parentStack)).toMatchSnapshot();
+  });
+
+  it("should error if the parent stack does not specify all parameters for the stack set instance template", () => {
+    const theStackSetInstance = new GuStackForStackSetInstance("the-stack-set", { stack: "test" });
+    new GuStringParameter(theStackSetInstance, "CentralSnsTopic", { allowedPattern: RegexPattern.ARN });
+
+    const parentStack = new GuStackForInfrastructure(new App(), "Test", { stack: "test" });
+
+    expect(() => {
+      new GuStackSet(parentStack, "StackSet", {
+        name: "my-stack-set",
+        description: "this stack set provisions some common infrastructure",
+        organisationUnitTargets: ["o-12345abcde"],
+        stackSetInstance: theStackSetInstance,
+      });
+    }).toThrow("There are undefined stack set parameters: CentralSnsTopic");
+  });
+});
