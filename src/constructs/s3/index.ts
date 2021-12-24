4c472ffdaa956c606ba5c7ef73ee6e695bad0d81
Akash Askoolum <akash1810@gmail.com>
2021-12-24 17:22:36 +0000

    fix: Block public access to S3 buckets by default

--- src/constructs/s3/index.ts
+++ src/constructs/s3/index.ts
@@ -1,5 +1,5 @@
 import type { BucketProps } from "@aws-cdk/aws-s3";
-import { Bucket } from "@aws-cdk/aws-s3";
+import { BlockPublicAccess, Bucket } from "@aws-cdk/aws-s3";
 import { RemovalPolicy } from "@aws-cdk/core";
 import { GuMigratableConstruct } from "../../utils/mixin";
 import type { GuStack } from "../core";
@@ -11,7 +11,12 @@ export interface GuS3BucketProps extends GuMigratingResource, Omit<BucketProps, 
  * A construct to create a bucket with a "retain" policy to prevent it from being deleted. It will be orphaned instead.
  */
 export class GuS3Bucket extends GuMigratableConstruct(Bucket) {
-  constructor(scope: GuStack, id: string, props: GuS3BucketProps) {
-    super(scope, id, { ...props, removalPolicy: RemovalPolicy.RETAIN });
+  constructor(scope: GuStack, id: string, props?: GuS3BucketProps) {
+    super(scope, id, {
+      removalPolicy: RemovalPolicy.RETAIN,
+      publicReadAccess: false,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+      ...props,
+    });
   }
 }
