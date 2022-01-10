bb34230d8abb6312e528da95fd9156099924ae6f
Jon Soul <jonathan.soul@guardian.co.uk>
2022-01-10 10:09:11 +0000

    Set prop defaults via destructuring

--- src/constructs/iam/policies/s3-put-object.ts
+++ src/constructs/iam/policies/s3-put-object.ts
@@ -9,7 +9,7 @@ export interface GuPutS3ObjectPolicyProps extends GuNoStatementsPolicyProps {
 
 export class GuPutS3ObjectsPolicy extends GuAllowPolicy {
   constructor(scope: GuStack, id: string, props: GuPutS3ObjectPolicyProps) {
-    const paths: string[] = props.paths ?? ["*"];
+    const { paths = ["*"] } = props; // set defaults
     const s3Resources: string[] = paths.map((path) => `arn:aws:s3:::${props.bucketName}/${path}`);
     super(scope, id, { ...props, actions: ["s3:PutObject"], resources: s3Resources });
   }
