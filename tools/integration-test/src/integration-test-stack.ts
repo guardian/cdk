b7e9be6d15f8465bf6c8899c2502edee14c8c00e
Akash Askoolum <akash1810@users.noreply.github.com>
2022-02-22 13:53:35 +0000

    test: Simplify integration project (#1114)
    #1113 is failing CI for two reasons:
      - AWS CDK library versions are too old (#1111 is raised to fix this)
      - The snapshot test in the integration project does not mock the library version tag, so with a new release, the value in the snapshot is out of date
    
    This change addresses the latter by simplifying the integration test. It reverts to the simple version that just added a parameter to a stack, rather than an entire pattern.
    
    This should unblock #1113.

--- tools/integration-test/src/integration-test-stack.ts
+++ tools/integration-test/src/integration-test-stack.ts
@@ -1,29 +1,10 @@
-import { InstanceClass, InstanceSize, InstanceType } from "@aws-cdk/aws-ec2";
 import type { App } from "@aws-cdk/core";
-import { GuPlayApp } from "@guardian/cdk";
-import { StageForInfrastructure } from "@guardian/cdk/lib/constants";
-import { AccessScope } from "@guardian/cdk/lib/constants/access";
+import { GuDistributionBucketParameter, GuStack } from "@guardian/cdk/lib/constructs/core";
 import type { GuStackProps } from "@guardian/cdk/lib/constructs/core";
-import { GuStackForInfrastructure } from "@guardian/cdk/lib/constructs/core";
 
-export class IntegrationTestStack extends GuStackForInfrastructure {
+export class IntegrationTestStack extends GuStack {
   constructor(scope: App, id: string, props: GuStackProps) {
     super(scope, id, props);
-    new GuPlayApp(this, {
-      app: "testing",
-      access: { scope: AccessScope.RESTRICTED, cidrRanges: [] },
-      instanceType: InstanceType.of(InstanceClass.T4G, InstanceSize.MEDIUM),
-      monitoringConfiguration: { noMonitoring: true },
-      userData: "#!/bin/dev foobarbaz",
-      certificateProps: {
-        [StageForInfrastructure]: {
-          domainName: "code-guardian.com",
-          hostedZoneId: "id123",
-        },
-      },
-      scaling: {
-        [StageForInfrastructure]: { minimumInstances: 1 },
-      },
-    });
+    GuDistributionBucketParameter.getInstance(this);
   }
 }
