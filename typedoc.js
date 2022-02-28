75397061e97f102d85563bb2aa3eedb92334b359
Akash Askoolum <akash1810@gmail.com>
2022-02-28 09:41:02 +0000

    docs(typedoc): Ignore directories w/out `index.ts`

--- typedoc.js
+++ typedoc.js
@@ -1,4 +1,4 @@
-const { readdirSync } = require("fs");
+const { readdirSync, existsSync } = require("fs");
 
 const constructsDir = "src/constructs";
 const utilsDir = "src/utils";
@@ -6,7 +6,8 @@ const utilsDir = "src/utils";
 function getEntryPointsFromSubdirectories(directory) {
   return readdirSync(directory, { withFileTypes: true })
     .filter((dir) => dir.isDirectory())
-    .map(({ name }) => `${directory}/${name}/index.ts`);
+    .map(({ name }) => `${directory}/${name}/index.ts`)
+    .filter((file) => existsSync(file));
 }
 
 module.exports = {
