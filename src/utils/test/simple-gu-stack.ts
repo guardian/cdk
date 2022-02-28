99514b7c519f68fac429321f7c1ae66720c84860
Akash Askoolum <akash1810@gmail.com>
2022-02-28 21:28:53 +0000

    chore: Resolve compile issues

--- src/utils/test/simple-gu-stack.ts
+++ src/utils/test/simple-gu-stack.ts
@@ -1,21 +1,13 @@
 import { App } from "@aws-cdk/core";
-import type { Environment } from "@aws-cdk/core";
-import { GuStack, GuStackForInfrastructure } from "../../constructs/core";
+import { GuStack } from "../../constructs/core";
 import type { GuStackProps } from "../../constructs/core";
 
-// Some stacks (such as ones using access logging on load balancers) require specifying a region
-interface SimpleGuStackProps extends Partial<GuStackProps> {
-  env?: Environment;
+export function simpleGuStackForTesting(props?: GuStackProps) {
+  // const { stack, stage } = props ?? { stack: "test-stack", stage: "TEST" };
+  const { stack, stage } = props ?? { stack: "test-stack", stage: "TEST" };
+  return new GuStack(new App(), "Test", {
+    stack,
+    stage,
+    ...props,
+  });
 }
-
-export const simpleGuStackForTesting: (props?: SimpleGuStackProps) => GuStack = (props?: SimpleGuStackProps) =>
-  new GuStack(new App(), "Test", {
-    stack: props?.stack ?? "test-stack",
-    ...props,
-  });
-
-export const simpleInfraStackForTesting: (props?: SimpleGuStackProps) => GuStack = (props?: SimpleGuStackProps) =>
-  new GuStackForInfrastructure(new App(), "Test", {
-    stack: props?.stack ?? "test-stack",
-    ...props,
-  });
