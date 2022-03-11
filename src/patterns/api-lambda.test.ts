a2a72c817ad3d1241a4f7fc10e36a3deca9e9471
George B <705427+georgeblahblah@users.noreply.github.com>
2022-03-11 10:46:20 +0000

    Test assigning a domain to a GuApiLambda

--- src/patterns/api-lambda.test.ts
+++ src/patterns/api-lambda.test.ts
@@ -1,7 +1,11 @@
 import "@aws-cdk/assert/jest";
 import { SynthUtils } from "@aws-cdk/assert";
+import { DomainName } from "@aws-cdk/aws-apigateway/lib/domain-name";
 import { Runtime } from "@aws-cdk/aws-lambda";
+import { Duration } from "@aws-cdk/core";
+import { GuCertificate } from "../constructs/acm";
 import type { NoMonitoring } from "../constructs/cloudwatch";
+import { GuCname } from "../constructs/dns";
 import { simpleGuStackForTesting } from "../utils/test";
 import { GuApiLambda } from "./api-lambda";
 
@@ -52,5 +56,45 @@ describe("The GuApiLambda pattern", () => {
       ],
     });
     expect(stack).toHaveResource("AWS::CloudWatch::Alarm");
+  });
+
+  it("should allow us to link a domain name to a LambdaRestApi", () => {
+    const stack = simpleGuStackForTesting();
+    const noMonitoring: NoMonitoring = { noMonitoring: true };
+    const apiLambda = new GuApiLambda(stack, "lambda", {
+      fileName: "my-app.zip",
+      handler: "handler.ts",
+      runtime: Runtime.NODEJS_12_X,
+      monitoringConfiguration: noMonitoring,
+      app: "testing",
+      apis: [
+        {
+          id: "api",
+          description: "this is a test",
+        },
+        {
+          id: "api2",
+          description: "this is a test2",
+        },
+      ],
+    });
+    const api = apiLambda.apis.get("api");
+    expect(api).toBeDefined();
+    api!.addDomainName("domain", {
+      domainName: "code.theguardian.com",
+      certificate: new GuCertificate(stack, {
+        app: "testing",
+        domainName: "code.theguardian.com",
+        hostedZoneId: "id123",
+      }),
+    });
+
+    new GuCname(stack, "DNS", {
+      app: "testing",
+      ttl: Duration.days(1),
+      domainName: "code.theguardian.com",
+      resourceRecord: api!.domainName!.domainNameAliasDomainName,
+    });
+    expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
   });
 });
