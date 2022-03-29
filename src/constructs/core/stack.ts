5c18e1fded1251a8ed21df25e6dadd7152fadb78
Akash Askoolum <akash1810@gmail.com>
2022-03-29 21:58:35 +0100

    refactor: Replace `SynthUtils` (removed) with `Template`

--- src/constructs/core/stack.ts
+++ src/constructs/core/stack.ts
@@ -1,6 +1,6 @@
-import { SynthUtils } from "@aws-cdk/assert";
 import { App, Stack, Tags } from "aws-cdk-lib";
 import type { StackProps } from "aws-cdk-lib";
+import { Template } from "aws-cdk-lib/assertions";
 import gitUrlParse from "git-url-parse";
 import { ContextKeys, TagKeys, TrackingTag } from "../../constants";
 import type { GuMigratingStack } from "../../types";
@@ -167,6 +167,6 @@ export class GuStackForStackSetInstance extends GuStack {
   }
 
   get cfnJson(): string {
-    return JSON.stringify(SynthUtils.toCloudFormation(this), null, 2);
+    return JSON.stringify(Template.fromStack(this).toJSON(), null, 2);
   }
 }
