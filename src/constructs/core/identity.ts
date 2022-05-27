c17a530a8f5cc0191c6e0d5fc3c379ce47df6a36
Akash Askoolum <akash1810@gmail.com>
2022-05-28 00:11:06 +0100

    fix: Remove now unused `GuAppAwareConstruct` mixin and `AppIdentity` functions

--- src/constructs/core/identity.ts
+++ src/constructs/core/identity.ts
@@ -1,6 +1,3 @@
-import { Tags } from "aws-cdk-lib";
-import type { IConstruct } from "constructs";
-
 export interface StackStageIdentity {
   stack: string;
   stage: string;
@@ -11,21 +8,3 @@ export interface AppIdentity {
 }
 
 export interface Identity extends StackStageIdentity, AppIdentity {}
-
-export const AppIdentity = {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any -- user defined type guard
-  isAppIdentity(props: any): props is AppIdentity {
-    return props ? "app" in props : false;
-  },
-
-  suffixText(appIdentity: AppIdentity, text: string): string {
-    const titleCaseApp = appIdentity.app.charAt(0).toUpperCase() + appIdentity.app.slice(1);
-    // CloudFormation Logical Ids must be alphanumeric, so remove any non-alphanumeric characters: https://stackoverflow.com/a/20864946
-    const alphanumericTitleCaseApp = titleCaseApp.replace(/[\W_]+/g, "");
-    return `${text}${alphanumericTitleCaseApp}`;
-  },
-  taggedConstruct<T extends IConstruct>(appIdentity: AppIdentity, construct: T): T {
-    Tags.of(construct).add("App", appIdentity.app);
-    return construct;
-  },
-};
--- src/utils/mixin/types.ts
+++
@@ -1,2 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types -- mixin
-export type AnyConstructor = new (...args: any[]) => {};
