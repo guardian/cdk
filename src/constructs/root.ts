ca16c873ed6f17707e2d2b1fc389578751a2a932
Akash Askoolum <akash1810@gmail.com>
2022-07-07 11:05:13 +0100

    feat(riff-raff.yaml): Introduce `GuRoot`
    The `GuRoot` class should allow for easy adoption of `riff-raff.yaml` generation.
    
    Going from:
    
    ```ts
    // bin/cdk.ts
    import { App } from "aws-cdk-lib";
    
    const app = new App();
    
    new MyStack(app, "some-id", {});
    ```
    
    To:
    
    ```ts
    // bin/cdk.ts
    import { GuRoot } from "@guardian/cdk/lib/constructs/root";
    
    const app = new GuRoot();
    
    new MyStack(app, "some-id", {});
    ```

---
+++ src/constructs/root.ts
@@ -0,0 +1,13 @@
+// Ideally this file would sit in `constructs/core`, however this creates violations of the `import/no-cycle` linting rule.
+import type { StageSynthesisOptions } from "aws-cdk-lib";
+import { App } from "aws-cdk-lib";
+import type { CloudAssembly } from "aws-cdk-lib/cx-api";
+import { RiffRaffYamlFile } from "../utils/riff-raff-yaml-file";
+
+// Deliberately not called `GuApp` as `app` is overloaded (as is `stack`, and `stage` 😅)
+export class GuRoot extends App {
+  override synth(options?: StageSynthesisOptions): CloudAssembly {
+    new RiffRaffYamlFile(this).synth();
+    return super.synth(options);
+  }
+}
