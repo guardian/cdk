c8207eded0805b8705e0bec8ab7977500c9f6a27
Mario Savarese <57295823+marsavar@users.noreply.github.com>
2023-06-20 14:24:51 +0100

    revert: Revert #1891
    * revert: revert #1891
    
    Passing subnets to the construct causes CDK to generate custom resources
    Reverting this while the issue is being investigated by AWS.

--- src/constructs/ecs/ecs-task.ts
+++ src/constructs/ecs/ecs-task.ts
@@ -1,7 +1,7 @@
 import { CfnOutput, Duration } from "aws-cdk-lib";
 import { Alarm, TreatMissingData } from "aws-cdk-lib/aws-cloudwatch";
 import { SnsAction } from "aws-cdk-lib/aws-cloudwatch-actions";
-import type { ISecurityGroup, ISubnet, IVpc } from "aws-cdk-lib/aws-ec2";
+import type { ISecurityGroup, IVpc } from "aws-cdk-lib/aws-ec2";
 import type { IRepository } from "aws-cdk-lib/aws-ecr";
 import {
   Cluster,
@@ -20,7 +20,6 @@ import { Construct } from "constructs";
 import type { NoMonitoring } from "../cloudwatch";
 import type { GuStack } from "../core";
 import { AppIdentity } from "../core";
-import { GuVpc, SubnetType } from "../ec2";
 import { GuGetDistributablePolicyStatement } from "../iam";
 
 /**
@@ -113,7 +112,6 @@ export interface GuEcsTaskProps extends AppIdentity {
   customTaskPolicies?: PolicyStatement[];
   environmentOverrides?: TaskEnvironmentVariable[];
   storage?: number;
-  subnets?: ISubnet[];
 }
 
 /**
@@ -138,7 +136,6 @@ const getContainer = (config: ContainerConfiguration) => {
  * Note that if your task reliably completes in less than 15 minutes then you should probably use a [[`GuLambda`]] instead. This
  * pattern was mainly created to work around the 15 minute lambda timeout.
  *
- * If the `subnets` prop is not defined, the task will run in a private subnet by default.
  */
 export class GuEcsTask extends Construct {
   stateMachine: StateMachine;
@@ -158,7 +155,6 @@ export class GuEcsTask extends Construct {
       taskTimeoutInMinutes = 15,
       customTaskPolicies,
       vpc,
-      subnets = GuVpc.subnetsFromParameter(scope, { type: SubnetType.PRIVATE, app }),
       monitoringConfiguration,
       securityGroups = [],
       environmentOverrides,
@@ -205,7 +201,6 @@ export class GuEcsTask extends Construct {
 
     const task = new EcsRunTask(scope, `${id}-task`, {
       cluster,
-      subnets: { subnets },
       launchTarget: new EcsFargateLaunchTarget({
         platformVersion: FargatePlatformVersion.LATEST,
       }),
