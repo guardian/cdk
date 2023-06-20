377a3c9bd3643335355a19ed080b5bd4c19c665c
Mario Savarese <57295823+marsavar@users.noreply.github.com>
2023-06-20 14:31:55 +0100

    feat: pass properties for an image hosted in a private registry

--- src/constructs/ecs/ecs-task.ts
+++ src/constructs/ecs/ecs-task.ts
@@ -3,6 +3,7 @@ import { Alarm, TreatMissingData } from "aws-cdk-lib/aws-cloudwatch";
 import { SnsAction } from "aws-cdk-lib/aws-cloudwatch-actions";
 import type { ISecurityGroup, IVpc } from "aws-cdk-lib/aws-ec2";
 import type { IRepository } from "aws-cdk-lib/aws-ecr";
+import type { RepositoryImageProps } from "aws-cdk-lib/aws-ecs";
 import {
   Cluster,
   Compatibility,
@@ -53,6 +54,7 @@ export type RepositoryContainer = {
 
 export type RegistryContainer = {
   id?: string;
+  imageProps?: RepositoryImageProps;
   type: "registry";
 };
 
@@ -122,7 +124,7 @@ const getContainer = (config: ContainerConfiguration) => {
   if (config.type == "repository") {
     return ContainerImage.fromEcrRepository(config.repository, config.version);
   } else {
-    return ContainerImage.fromRegistry(config.id ?? "ubuntu:focal");
+    return ContainerImage.fromRegistry(config.id ?? "ubuntu:jammy", config.imageProps);
   }
 };
 
