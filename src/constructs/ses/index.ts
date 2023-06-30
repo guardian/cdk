ad2b1ab1189c91c65bbe31c647fbeca5f77c1b59
Robert Kenny <kenoir@gmail.com>
2023-06-30 10:07:10 +0100

    dont force splitting domain name, keep it simple

--- src/constructs/ses/index.ts
+++ src/constructs/ses/index.ts
@@ -5,20 +5,20 @@ import type { AppIdentity, GuStack } from "../core";
 import { GuCname } from "../dns";
 
 export interface GuEmailIdentityProps extends Omit<EmailIdentityProps, "identity">, AppIdentity {
-  guToolsSubdomain: string;
+  domainName: string;
 }
 
 export class GuEmailIdentity extends EmailIdentity {
   constructor(scope: GuStack, id: string, props: GuEmailIdentityProps) {
+    const { app, domainName } = props;
     super(scope, id, {
-      // Auto verification is only supported for gutools.co.uk domains
-      identity: Identity.domain(`${props.guToolsSubdomain}.gutools.co.uk`),
+      identity: Identity.domain(domainName),
       ...props,
     });
 
     this.dkimRecords.forEach(({ name, value }, index) => {
       new GuCname(scope, `EmailIdentityDkim${index}`, {
-        app: props.app,
+        app,
         domainName: name,
         resourceRecord: value,
         ttl: Duration.hours(1),
