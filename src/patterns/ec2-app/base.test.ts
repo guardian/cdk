ac7354f47a575fd4e1c2f313d1e594f44d6981af
Mario Savarese <mario.savarese@guardian.co.uk>
2024-01-29 15:49:16 +0000

    feat: Support more than one EC2 app with load balancer access logs enabled

--- src/patterns/ec2-app/base.test.ts
+++ src/patterns/ec2-app/base.test.ts
@@ -934,84 +934,142 @@ describe("the GuEC2App pattern", function () {
         }),
     ).toThrowError("googleAuth.allowedGroups must use the @guardian.co.uk domain.");
   });
-});
 
-it("should provides a default healthcheck", function () {
-  const stack = simpleGuStackForTesting();
-  new GuEc2App(stack, {
-    applicationPort: 3000,
-    app: "test-gu-ec2-app",
-    access: { scope: AccessScope.PUBLIC },
-    instanceType: InstanceType.of(InstanceClass.T4G, InstanceSize.MEDIUM),
-    monitoringConfiguration: { noMonitoring: true },
-    userData: "#!/bin/dev foobarbaz",
-    certificateProps: {
-      domainName: "domain-name-for-your-application.example",
-    },
-    scaling: {
-      minimumInstances: 1,
-    },
-  });
-  Template.fromStack(stack).hasResourceProperties("AWS::ElasticLoadBalancingV2::TargetGroup", {
-    HealthCheckIntervalSeconds: 10,
-    HealthCheckPath: "/healthcheck",
-    HealthCheckProtocol: "HTTP",
-    HealthCheckTimeoutSeconds: 5,
-    HealthyThresholdCount: 5,
-  });
-});
-
-it("allows a custom healthcheck", function () {
-  const stack = simpleGuStackForTesting();
-  new GuEc2App(stack, {
-    applicationPort: 3000,
-    app: "test-gu-ec2-app",
-    access: { scope: AccessScope.PUBLIC },
-    instanceType: InstanceType.of(InstanceClass.T4G, InstanceSize.MEDIUM),
-    monitoringConfiguration: { noMonitoring: true },
-    userData: "#!/bin/dev foobarbaz",
-    certificateProps: {
-      domainName: "domain-name-for-your-application.example",
-    },
-    scaling: {
-      minimumInstances: 1,
-    },
-    healthcheck: {
-      path: "/custom-healthcheck",
-    },
-  });
-  Template.fromStack(stack).hasResourceProperties("AWS::ElasticLoadBalancingV2::TargetGroup", {
-    HealthCheckIntervalSeconds: 10,
-    HealthCheckPath: "/custom-healthcheck",
-    HealthCheckProtocol: "HTTP",
-    HealthCheckTimeoutSeconds: 5,
-    HealthyThresholdCount: 5,
-  });
-});
-
-it("can specify instance metadata hop limit", function () {
-  const stack = simpleGuStackForTesting();
-  new GuEc2App(stack, {
-    applicationPort: 3000,
-    app: "test-gu-ec2-app",
-    access: { scope: AccessScope.PUBLIC },
-    instanceType: InstanceType.of(InstanceClass.T4G, InstanceSize.MEDIUM),
-    monitoringConfiguration: { noMonitoring: true },
-    userData: "#!/bin/dev foobarbaz",
-    certificateProps: {
-      domainName: "domain-name-for-your-application.example",
-    },
-    scaling: {
-      minimumInstances: 1,
-    },
-    instanceMetadataHopLimit: 2,
-  });
-  Template.fromStack(stack).hasResourceProperties("AWS::EC2::LaunchTemplate", {
-    LaunchTemplateData: {
-      MetadataOptions: {
-        HttpPutResponseHopLimit: 2,
-        HttpTokens: "required",
+  it("should provides a default healthcheck", function () {
+    const stack = simpleGuStackForTesting();
+    new GuEc2App(stack, {
+      applicationPort: 3000,
+      app: "test-gu-ec2-app",
+      access: { scope: AccessScope.PUBLIC },
+      instanceType: InstanceType.of(InstanceClass.T4G, InstanceSize.MEDIUM),
+      monitoringConfiguration: { noMonitoring: true },
+      userData: "#!/bin/dev foobarbaz",
+      certificateProps: {
+        domainName: "domain-name-for-your-application.example",
       },
-    },
+      scaling: {
+        minimumInstances: 1,
+      },
+    });
+    Template.fromStack(stack).hasResourceProperties("AWS::ElasticLoadBalancingV2::TargetGroup", {
+      HealthCheckIntervalSeconds: 10,
+      HealthCheckPath: "/healthcheck",
+      HealthCheckProtocol: "HTTP",
+      HealthCheckTimeoutSeconds: 5,
+      HealthyThresholdCount: 5,
+    });
+  });
+
+  it("allows a custom healthcheck", function () {
+    const stack = simpleGuStackForTesting();
+    new GuEc2App(stack, {
+      applicationPort: 3000,
+      app: "test-gu-ec2-app",
+      access: { scope: AccessScope.PUBLIC },
+      instanceType: InstanceType.of(InstanceClass.T4G, InstanceSize.MEDIUM),
+      monitoringConfiguration: { noMonitoring: true },
+      userData: "#!/bin/dev foobarbaz",
+      certificateProps: {
+        domainName: "domain-name-for-your-application.example",
+      },
+      scaling: {
+        minimumInstances: 1,
+      },
+      healthcheck: {
+        path: "/custom-healthcheck",
+      },
+    });
+    Template.fromStack(stack).hasResourceProperties("AWS::ElasticLoadBalancingV2::TargetGroup", {
+      HealthCheckIntervalSeconds: 10,
+      HealthCheckPath: "/custom-healthcheck",
+      HealthCheckProtocol: "HTTP",
+      HealthCheckTimeoutSeconds: 5,
+      HealthyThresholdCount: 5,
+    });
+  });
+
+  it("can specify instance metadata hop limit", function () {
+    const stack = simpleGuStackForTesting();
+    new GuEc2App(stack, {
+      applicationPort: 3000,
+      app: "test-gu-ec2-app",
+      access: { scope: AccessScope.PUBLIC },
+      instanceType: InstanceType.of(InstanceClass.T4G, InstanceSize.MEDIUM),
+      monitoringConfiguration: { noMonitoring: true },
+      userData: "#!/bin/dev foobarbaz",
+      certificateProps: {
+        domainName: "domain-name-for-your-application.example",
+      },
+      scaling: {
+        minimumInstances: 1,
+      },
+      instanceMetadataHopLimit: 2,
+    });
+    Template.fromStack(stack).hasResourceProperties("AWS::EC2::LaunchTemplate", {
+      LaunchTemplateData: {
+        MetadataOptions: {
+          HttpPutResponseHopLimit: 2,
+          HttpTokens: "required",
+        },
+      },
+    });
+  });
+
+  it("supports more than one EC2 app with load balancer access logs enabled", () => {
+    const stack = simpleGuStackForTesting({
+      env: {
+        region: "test",
+      },
+    });
+
+    new GuEc2App(stack, {
+      applicationPort: 3000,
+      app: "test-gu-ec2-app-1",
+      access: { scope: AccessScope.PUBLIC },
+      instanceType: InstanceType.of(InstanceClass.T4G, InstanceSize.MEDIUM),
+      monitoringConfiguration: { noMonitoring: true },
+      userData: "#!/bin/dev foobarbaz",
+      certificateProps: {
+        domainName: "domain-name-for-your-application.example",
+      },
+      scaling: {
+        minimumInstances: 1,
+      },
+      instanceMetadataHopLimit: 2,
+      accessLogging: {
+        enabled: true,
+        prefix: "test-1",
+      },
+    });
+
+    new GuEc2App(stack, {
+      applicationPort: 3000,
+      app: "test-gu-ec2-app-2",
+      access: { scope: AccessScope.PUBLIC },
+      instanceType: InstanceType.of(InstanceClass.T4G, InstanceSize.MEDIUM),
+      monitoringConfiguration: { noMonitoring: true },
+      userData: "#!/bin/dev foobarbaz",
+      certificateProps: {
+        domainName: "domain-name-for-your-application.example",
+      },
+      scaling: {
+        minimumInstances: 1,
+      },
+      instanceMetadataHopLimit: 2,
+      accessLogging: {
+        enabled: true,
+        prefix: "test-2",
+      },
+    });
+
+    Template.fromStack(stack).hasResourceProperties("AWS::ElasticLoadBalancingV2::LoadBalancer", {
+      Tags: Match.arrayWith([Match.objectLike({ Key: "App", Value: "test-gu-ec2-app-1" })]),
+      LoadBalancerAttributes: Match.arrayWith([Match.objectLike({ Key: "access_logs.s3.prefix", Value: "test-1" })]),
+    });
+
+    Template.fromStack(stack).hasResourceProperties("AWS::ElasticLoadBalancingV2::LoadBalancer", {
+      Tags: Match.arrayWith([Match.objectLike({ Key: "App", Value: "test-gu-ec2-app-2" })]),
+      LoadBalancerAttributes: Match.arrayWith([Match.objectLike({ Key: "access_logs.s3.prefix", Value: "test-2" })]),
+    });
   });
 });
