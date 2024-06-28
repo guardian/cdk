7b5d40e81b910771e7c9817bba177e87e6583fc8
Ashleigh Carr <ashcorr20@gmail.com>
2024-06-28 12:07:56 +0100

    Image distributions need to specify amiDistributionConfig, even if its empty

--- src/constructs/imagebuilder/distribution.ts
+++ src/constructs/imagebuilder/distribution.ts
@@ -22,6 +22,7 @@ export class GuDistributionConfiguration extends CfnDistributionConfiguration {
       distributions: props.distributions ?? [
         {
           region: scope.region,
+          amiDistributionConfiguration: {},
         },
       ],
     });
