94ac502ae611024ff66076c80b09453fd43792b9
akash1810 <akash1810@users.noreply.github.com>
2024-07-29 20:53:08 +0100

    chore: Prettier changes

--- src/riff-raff-yaml-file/deployments/cloudformation.ts
+++ src/riff-raff-yaml-file/deployments/cloudformation.ts
@@ -59,10 +59,10 @@ export function addAmiParametersToCloudFormationDeployment(
     }
 
     const Recipe = typeof imageRecipe === "string" ? imageRecipe : imageRecipe.Recipe;
-    const AmigoStage = typeof imageRecipe === "string" ? "PROD" : imageRecipe.AmigoStage ?? "PROD";
+    const AmigoStage = typeof imageRecipe === "string" ? "PROD" : (imageRecipe.AmigoStage ?? "PROD");
 
     // In YAML `true` is a Boolean. Riff-Raff expects a String here, so call `toString` to make it `'true'`.
-    const Encrypted = (typeof imageRecipe === "string" ? true : imageRecipe.Encrypted ?? true).toString();
+    const Encrypted = (typeof imageRecipe === "string" ? true : (imageRecipe.Encrypted ?? true)).toString();
 
     return {
       ...acc,
