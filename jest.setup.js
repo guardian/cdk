67dda1dd1a56ae74e6b001234c364473883ca0af
AshCorr <ashcorr20@gmail.com>
2025-04-09 11:19:39 +0100

    feat: Upgrade to ESLint 9.x

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,3 @@
+/* eslint-disable no-undef -- jest globals are missing due to guardian.recommended.jest eslint rules not covering jest.setup.js */
 jest.mock("./src/constants/tracking-tag");
 jest.mock("./src/constants/library-info");
