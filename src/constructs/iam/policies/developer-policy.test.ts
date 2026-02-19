8de29e8fe7a6504079064fdb2adac21901c69bf6
Justin Rowles <justin.rowles.contractor@guardian.co.uk>
2026-02-19 14:33:43 +0000

    Create "raw" managed policy using existing class props

---
+++ src/constructs/iam/policies/developer-policy.test.ts
@@ -0,0 +1,28 @@
+import { Template } from "aws-cdk-lib/assertions";
+import { simpleGuStackForTesting } from "../../../utils/test";
+import { GuDeveloperPolicy } from "./developer-policy";
+
+describe("GuManagedPolicy", () => {
+  test("if a single action is provided, the resulting Managed Policy resource's action will be a single item", () => {
+    const stack = simpleGuStackForTesting();
+    new GuDeveloperPolicy(stack, "AllowS3GetObject", {
+      allow: {
+        actions: ["s3:GetObject"],
+        resources: ["*"],
+      },
+    });
+
+    Template.fromStack(stack).hasResourceProperties("AWS::IAM::ManagedPolicy", {
+      PolicyDocument: {
+        Version: "2012-10-17",
+        Statement: [
+          {
+            Action: "s3:GetObject",
+            Effect: "Allow",
+            Resource: "*",
+          },
+        ],
+      },
+    });
+  });
+});
