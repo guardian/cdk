379d8ab905bad4bab499848c30409897b45cf43c
Tom Wey <tom.wey@theguardian.com>
2026-04-14 12:25:49 +0100

    Pass shell: false to childProcess.spawn
    This is more secure. After upgrading a project to Node 24 I started
    seeing deprecation warnings like this, coming from @guardian/cdk:
    
    DeprecationWarning: Passing args to a child process with shell option
    true can lead to security vulnerabilities, as the arguments are not
    escaped, only concatenated.

--- src/utils/exec.ts
+++ src/utils/exec.ts
@@ -4,7 +4,7 @@ import childProcess from "child_process";
 export async function execute(cmd: string, args: string[], { cwd }: { cwd: string }): Promise<string> {
   const child = childProcess.spawn(cmd, args, {
     cwd,
-    shell: true,
+    shell: false,
     stdio: ["ignore", "pipe", "inherit"],
   });
 
