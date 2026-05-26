4eb24fbcdf4bbbf2ca1882dba193b478832ac3f5
Jacob Winch <jacob.winch@guardian.co.uk>
2026-05-26 12:21:58 +0100

    Fix docs

--- src/experimental/patterns/gu-load-balanced-app.ts
+++ src/experimental/patterns/gu-load-balanced-app.ts
@@ -69,9 +69,8 @@ export interface GuLoadBalancedAppExperimentalProps extends AppIdentity {
   /**
    * Network access restrictions for your load balancer.
    *
-   * Note, this merely provides defence in depth; you should, for example, limit
-   * access to the VPN and then treat that as sufficient. Instead, use Google
-   * Auth for human access, or a suitable machine auth mechanism.
+   * Note, this merely provides defence in depth; you should NOT rely on network access restrictions alone for
+   * restricting access. Use Google Auth for human access, or a suitable machine auth mechanism.
    */
   access: AppAccess;
   /**
