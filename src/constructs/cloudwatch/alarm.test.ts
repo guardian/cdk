32217c33f0f47ea9ee1030a3ea4492277e4099d3
Justin Rowles <justin.rowles.contractor@guardian.co.uk>
2026-07-31 16:50:16 +0100

    Add log link to alarms

--- src/constructs/cloudwatch/alarm.test.ts
+++ src/constructs/cloudwatch/alarm.test.ts
@@ -4,7 +4,7 @@ import { Runtime } from "aws-cdk-lib/aws-lambda";
 import { simpleGuStackForTesting } from "../../utils/test";
 import type { GuStack } from "../core";
 import { GuLambdaFunction } from "../lambda";
-import { GuAlarm } from "./alarm";
+import { GuAlarm, GuAlarmCta } from "./alarm";
 
 describe("The GuAlarm class", () => {
   const lambda = (stack: GuStack) =>
@@ -91,5 +91,32 @@ describe("The GuAlarm class", () => {
     Template.fromStack(stack).hasResourceProperties("AWS::CloudWatch::Alarm", {
       OKActions: snsActionsCFN,
     });
+  });
+
+  it("should generate the correct log link from an elk space", () => {
+    const scope = simpleGuStackForTesting({ app: "myapp" });
+    const testLink = new GuAlarmCta(scope, {
+      elkSpace: "aaa",
+    }).ctaLinks;
+    expect(testLink).toEqual([
+      "https://logs.gutools.co.uk/s/aaa/app/discover#/?" +
+        "_g=(filters:!(),refreshInterval:(pause:!t,value:60000),time:(from:now-1d,to:now))" +
+        "&" +
+        "_a=(" +
+        "columns:!(stack,stage,message,app)," +
+        "filters:!(" +
+        "('$state':(store:appState),meta:(alias:!n,disabled:!f,key:stack.keyword,negate:!f,params:(query:test-stack),type:phrase),query:(match_phrase:(stack.keyword:test-stack)))," +
+        "('$state':(store:appState),meta:(alias:!n,disabled:!f,key:app.keyword,negate:!f,params:(query:myapp),type:phrase),query:(match_phrase:(app.keyword:myapp)))," +
+        "('$state':(store:appState),meta:(alias:!n,disabled:!f,key:stage.keyword,negate:!f,params:(query:TEST),type:phrase),query:(match_phrase:(stage.keyword:TEST))))," +
+        "hideChart:!t,interval:auto,query:(language:kuery,query:exception),sort:!(!('@timestamp',desc)))",
+    ]);
+  });
+
+  it("should generate the correct log link from a link", () => {
+    const scope = simpleGuStackForTesting();
+    const testLink = new GuAlarmCta(scope, {
+      link: "https://www.example.com",
+    }).ctaLinks;
+    expect(testLink).toEqual(["https://www.example.com"]);
   });
 });
